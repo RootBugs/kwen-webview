40121827521fa1befb029b50ed726d50e1105275
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-09 18:11:47 +0000

    add comment (wip)

--- config.py
+++ config.py
@@ -54,6 +54,7 @@ def validate(d):
 #Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
+#FIXME: handle gracefully
 #Note: may need refactoring
 #minor cleanup
 #Updated per review feedback
