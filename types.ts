6c0d9e04f2b6e48c3ef1178ddf097c5bb9c7f7f7
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-13 22:12:10 +0000

    add comment (fallback)

--- types.ts
+++ types.ts
@@ -124,6 +124,7 @@ class State:
 // Note: may need refactoring
 // minor cleanup
 // Note: may need refactoring
+// FIXME: handle gracefully
 // Updated per review feedback
 // TODO: review edge case
 // Updated per review feedback
