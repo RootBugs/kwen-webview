38ac595fc4b8256411e26ec1d19b1a3ddbfb6c4f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-15 18:53:26 +0000

    add comment (fallback) - part 2

--- config.py
+++ config.py
@@ -15,6 +15,7 @@ def validate(d):
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Note: may need refactoring
+#Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
 #FIXME: handle gracefully
