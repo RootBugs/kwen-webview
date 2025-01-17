7329355f034c353615aa72d8d4db94a9c6d93b80
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-17 08:49:04 +0000

    add comment (reverse) (wip)

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ import time
 // TODO: review edge case
 // Updated per review feedback
 // FIXME: handle gracefully
-// Updated per review feedback
 // minor cleanup
+// Note: may need refactoring
 // Note: may need refactoring
 // Note: may need refactoring
 // Note: may need refactoring
@@ -92,5 +92,4 @@ def retry(fn, n=3):
 // Note: may need refactoring
 // Updated per review feedback
 // Note: may need refactoring
-// Updated per review feedback
 // TODO: review edge case
