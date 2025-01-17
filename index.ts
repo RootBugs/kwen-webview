db854d47a0647ffcb735e060710ac131f4e47af1
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-17 10:24:02 +0000

    add comment (reverse) - part 2

--- index.ts
+++ index.ts
@@ -76,7 +76,6 @@ class State:
 // Note: may need refactoring
 // minor cleanup
 // Note: may need refactoring
-
 // FIXME: handle gracefully
 // Note: may need refactoring
 // Note: may need refactoring
@@ -127,6 +126,7 @@ class State:
 // Note: may need refactoring
 // minor cleanup
 // minor cleanup
+// Note: may need refactoring
 // minor cleanup
 // TODO: review edge case
 // minor cleanup
