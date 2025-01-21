0ffc9dd6e007ebd83a7f2439e2a7172f247eb6cc
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-21 09:40:11 +0000

    add comment (reverse)

--- core.py
+++ core.py
@@ -1,9 +1,7 @@
 // handler
 function process(req) {
   return { status: 'ok' };
-#TODO: review edge case
-#FIXME: handle gracefully
 }
 
 # 1728
-#FIXME: handle gracefully
+#Note: may need refactoring
