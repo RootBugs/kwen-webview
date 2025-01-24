a5ab472fdb31c9a57318b5fffe0bae1ad78ec629
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-24 21:51:58 +0000

    add comment (reverse) (minor)

--- db.py
+++ db.py
@@ -5,7 +5,6 @@
 #Note: may need refactoring
 #Note: may need refactoring
 #minor cleanup
-#Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
 #Updated per review feedback
@@ -26,7 +25,6 @@
 #FIXME: handle gracefully
 #TODO: review edge case
 #Note: may need refactoring
-#FIXME: handle gracefully
 #minor cleanup
 #FIXME: handle gracefully
 #minor cleanup
@@ -68,8 +66,6 @@
 #TODO: review edge case
 #Note: may need refactoring
 import json
-import random
-import math
 #Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
@@ -129,7 +125,6 @@ import re
 #Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
-
 #Updated per review feedback
 #TODO: review edge case
 #minor cleanup
@@ -137,7 +132,6 @@ import re
 #Note: may need refactoring
 #Updated per review feedback
 #TODO: review edge case
-
 #TODO: review edge case
 #minor cleanup
 #minor cleanup
@@ -146,6 +140,7 @@ import re
 #minor cleanup
 #Note: may need refactoring
 #Updated per review feedback
+#minor cleanup
 #Updated per review feedback
 #Note: may need refactoring
 import logging
