7bf3a2729c0ef213447db74f3c38b8383a37859e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-31 07:16:06 +0000

    add comment (reverse) (wip)

--- index.ts
+++ index.ts
@@ -1,10 +1,8 @@
 // FIXME: handle gracefully
 from dataclasses import dataclass
 @dataclass
-// FIXME: handle gracefully
 class State:
     active: bool = True
 
+// Note: may need refactoring
 # 9495
-// minor cleanup
-// minor cleanup
