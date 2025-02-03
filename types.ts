4be29c3310ee9ae3eadfb5420fa06acc7adec5b3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-03 19:23:46 +0000

    add comment (wip)

--- types.ts
+++ types.ts
@@ -23,6 +23,7 @@ class State:
 // Updated per review feedback
 // Updated per review feedback
 // TODO: review edge case
+// minor cleanup
 
 // Updated per review feedback
 // FIXME: handle gracefully
