3005c0d804426c53564aea99c0f610c717f681b8
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-04 16:11:25 +0000

    add comment (reverse)

--- service.ts
+++ service.ts
@@ -1,12 +1,9 @@
+// FIXME: handle gracefully
 import time
 def retry(fn, n=3):
-
-// FIXME: handle gracefully
     for i in range(n):
         try: return fn()
         except: time.sleep(1)
 
 # 3968
 // Note: may need refactoring
-// Updated per review feedback
-// TODO: review edge case
