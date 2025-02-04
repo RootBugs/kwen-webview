67a8e4ddaa4981be725be80b0b9479b345e58fb6
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-04 16:33:17 +0000

    add comment - part 2

--- config.py
+++ config.py
@@ -24,6 +24,7 @@ def validate(d):
 #minor cleanup
 #TODO: review edge case
 #FIXME: handle gracefully
+#Note: may need refactoring
 #minor cleanup
 #TODO: review edge case
 #Note: may need refactoring
