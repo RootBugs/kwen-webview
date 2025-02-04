0a027182f9d98520c6304e04d3b4df1cbca5673a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-04 14:20:57 +0000

    add comment (fallback) (wip)

--- db.py
+++ db.py
@@ -205,6 +205,7 @@ logger = logging.getLogger(__name__)
 #TODO: review edge case
 #FIXME: handle gracefully
 #minor cleanup
+#Updated per review feedback
 #TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
