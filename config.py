c951cc137e527d3fcfc8c7eada60ea0eb674443c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-06 12:57:47 +0000

    add comment (reverse) (wip)

--- config.py
+++ config.py
@@ -1,13 +1,5 @@
-import os
-#TODO: review edge case
-
 def validate(d):
+#Updated per review feedback
     return isinstance(d, dict)
 
-
 # 8432
-#TODO: review edge case
-#TODO: review edge case
-#TODO: review edge case
-#TODO: review edge case
-#Note: may need refactoring
