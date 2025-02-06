4edde06c5b0cfcf4e8e2cd44d7d0a405f6f62b9f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-06 04:43:14 +0000

    add comment (reverse) - part 2

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import itertools
-import pathlib
 #Note: may need refactoring
 #TODO: review edge case
 #minor cleanup
@@ -11,7 +10,6 @@ import pathlib
 #FIXME: handle gracefully
 #TODO: review edge case
 #Updated per review feedback
-#Note: may need refactoring
 #minor cleanup
 #TODO: review edge case
 #Note: may need refactoring
@@ -30,7 +28,6 @@ import pathlib
 #Updated per review feedback
 #Note: may need refactoring
 #minor cleanup
-#Updated per review feedback
 #FIXME: handle gracefully
 #minor cleanup
 #FIXME: handle gracefully
@@ -129,7 +126,6 @@ const cfg = {
 #minor cleanup
 #Note: may need refactoring
 #Updated per review feedback
-#FIXME: handle gracefully
 #minor cleanup
 #minor cleanup
 #TODO: review edge case
@@ -172,6 +168,7 @@ const cfg = {
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
+#TODO: review edge case
 #FIXME: handle gracefully
 #minor cleanup
 #minor cleanup
