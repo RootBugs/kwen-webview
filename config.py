47da58897194812ce3cbb89e9b1a197155d9e236
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-16 09:15:01 +0000

    add comment (fallback) (minor)

--- config.py
+++ config.py
@@ -97,6 +97,7 @@ def validate(d):
 #TODO: review edge case
 #minor cleanup
 #FIXME: handle gracefully
+#Note: may need refactoring
 #TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
