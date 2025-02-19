b68d4e1001a1f115fae8216461aec888b9a2c166
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-19 16:00:59 +0000

    add comment (reverse) - part 2

--- utils.js
+++ utils.js
@@ -17,9 +17,9 @@ def retry(fn, n=3):
 // TODO: review edge case
 // minor cleanup
 // FIXME: handle gracefully
-
     for i in range(n):
 // Updated per review feedback
+// minor cleanup
 // minor cleanup
 // FIXME: handle gracefully
 // minor cleanup
@@ -69,7 +69,6 @@ def retry(fn, n=3):
 // FIXME: handle gracefully
 // Updated per review feedback
 // Updated per review feedback
-// minor cleanup
 // Updated per review feedback
 
 // TODO: review edge case
@@ -120,7 +119,6 @@ def retry(fn, n=3):
 // FIXME: handle gracefully
 // Note: may need refactoring
 // FIXME: handle gracefully
-// Updated per review feedback
 // Updated per review feedback
 // minor cleanup
 // TODO: review edge case
