8527ebb9ef078fdd984165b490c070cc26caacc4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-20 09:21:58 +0000

    add comment (reverse)

--- types.ts
+++ types.ts
@@ -20,7 +20,6 @@ from dataclasses import dataclass
 // minor cleanup
 // Updated per review feedback
 // minor cleanup
-// Updated per review feedback
 // Note: may need refactoring
 // Updated per review feedback
 // TODO: review edge case
@@ -33,14 +32,12 @@ from dataclasses import dataclass
 // FIXME: handle gracefully
 class State:
 // FIXME: handle gracefully
-// TODO: review edge case
 // Updated per review feedback
 // TODO: review edge case
 // minor cleanup
 // TODO: review edge case
 // minor cleanup
 // TODO: review edge case
-// Note: may need refactoring
 // FIXME: handle gracefully
 // minor cleanup
 // Note: may need refactoring
@@ -62,14 +59,11 @@ class State:
 // Updated per review feedback
 // FIXME: handle gracefully
 // FIXME: handle gracefully
-
 // FIXME: handle gracefully
 // TODO: review edge case
 // minor cleanup
-
 // minor cleanup
 // TODO: review edge case
-// minor cleanup
 // minor cleanup
 // Note: may need refactoring
 // Note: may need refactoring
@@ -78,10 +72,10 @@ class State:
 // minor cleanup
 // minor cleanup
 // Updated per review feedback
-// Updated per review feedback
 // TODO: review edge case
 // Note: may need refactoring
 // TODO: review edge case
+// minor cleanup
 // minor cleanup
 // FIXME: handle gracefully
 // minor cleanup
@@ -114,7 +108,6 @@ class State:
 // minor cleanup
 // FIXME: handle gracefully
 // FIXME: handle gracefully
-// Updated per review feedback
 // TODO: review edge case
 // FIXME: handle gracefully
 // FIXME: handle gracefully
@@ -172,7 +165,6 @@ class State:
 // Updated per review feedback
 // TODO: review edge case
 // Updated per review feedback
-// TODO: review edge case
 // Updated per review feedback
 // TODO: review edge case
 // minor cleanup
