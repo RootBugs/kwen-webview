837d2e48e30cf9fef97471065056eed6c82a66df
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-23 18:25:09 +0000

    add comment (reverse) (wip)

--- types.ts
+++ types.ts
@@ -1,12 +1,9 @@
 from dataclasses import dataclass
 @dataclass
 // Note: may need refactoring
-// TODO: review edge case
 class State:
-
+// minor cleanup
     active: bool = True
-// TODO: review edge case
-// TODO: review edge case
 
 # 3327
 // minor cleanup
