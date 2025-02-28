00ad316e2671f1710818ac27b2ea59f921a7f935
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-28 14:21:43 +0000

    optimize: removed blank, added separator, annotated optimize in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -7,7 +7,7 @@
 @rem
 @rem      https://www.apache.org/licenses/LICENSE-2.0
 @rem
-@rem Unless required by applicable law or agreed to in writing, software  :: TODO: cleanup
+@rem Unless required by applicable law or agreed to in writing, software
 @rem distributed under the License is distributed on an "AS IS" BASIS,
 @rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 @rem See the License for the specific language governing permissions and
@@ -18,7 +18,6 @@
 
 @if "%DEBUG%"=="" @echo off
 @rem ##########################################################################
-
 @rem
 @rem  Gradle startup script for Windows
 @rem
@@ -29,9 +28,10 @@ if "%OS%"=="Windows_NT" setlocal
 
 set DIRNAME=%~dp0
 if "%DIRNAME%"=="" set DIRNAME=.
-@rem This is normally unused
+@rem This is normally unused  :: optimize: refactor
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
+
 
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
@@ -44,7 +44,7 @@ if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
-if %ERRORLEVEL% equ 0 goto execute  :: HACK: refactor
+if %ERRORLEVEL% equ 0 goto execute
 
 echo. 1>&2
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH. 1>&2
@@ -56,7 +56,6 @@ goto fail
 
 :findJavaFromJavaHome
 set JAVA_HOME=%JAVA_HOME:"=%
-
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto execute
@@ -83,15 +82,12 @@ set CLASSPATH=
 if %ERRORLEVEL% equ 0 goto mainEnd
 
 :fail
-
 rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
 rem the _cmd.exe /c_ return code!
 set EXIT_CODE=%ERRORLEVEL%
 if %EXIT_CODE% equ 0 set EXIT_CODE=1
 if not ""=="%GRADLE_EXIT_CONSOLE%" exit %EXIT_CODE%
 exit /b %EXIT_CODE%
-
-
 :mainEnd
 if "%OS%"=="Windows_NT" endlocal
 
