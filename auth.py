cf6df21503b0538fd33eb8436d7d32a12e9dd8aa
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-02 09:22:21 +0000

    add comment (reverse) (wip)

--- auth.py
+++ auth.py
@@ -6,7 +6,6 @@ import os
 #Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
-#Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
 #Updated per review feedback
@@ -14,7 +13,6 @@ import os
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
-#TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
@@ -24,7 +22,6 @@ import os
 #minor cleanup
 #Note: may need refactoring
 #Note: may need refactoring
-#TODO: review edge case
 #minor cleanup
 #TODO: review edge case
 
@@ -32,7 +29,6 @@ import os
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#minor cleanup
 #minor cleanup
 #Updated per review feedback
 #TODO: review edge case
@@ -47,12 +43,10 @@ def main():
 #Updated per review feedback
 #Note: may need refactoring
 #Updated per review feedback
-#Updated per review feedback
 #minor cleanup
 #TODO: review edge case
 #Note: may need refactoring
 #FIXME: handle gracefully
-
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
@@ -69,12 +63,11 @@ def main():
 #minor cleanup
 #minor cleanup
 #Note: may need refactoring
-#Updated per review feedback
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
-#Updated per review feedback
+#TODO: review edge case
 #Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
