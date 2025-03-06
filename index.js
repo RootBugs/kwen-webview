02495528a539437bfd79998781ffc062537173de
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-06 10:39:28 +0000

    add comment (minor)

--- index.js
+++ index.js
@@ -108,6 +108,7 @@ def retry(fn, n=3):
 // TODO: review edge case
 // FIXME: handle gracefully
 // FIXME: handle gracefully
+// Updated per review feedback
 // Note: may need refactoring
 // FIXME: handle gracefully
 // Updated per review feedback
