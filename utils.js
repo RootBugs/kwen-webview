e57bbd33d612af687244320a5933c1ad4e68f842
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-08 10:35:59 +0000

    add comment (reverse)

--- utils.js
+++ utils.js
@@ -1,16 +1,12 @@
 import time
 def retry(fn, n=3):
 // Updated per review feedback
-// Note: may need refactoring
     for i in range(n):
-
 // minor cleanup
 // TODO: review edge case
-// Note: may need refactoring
+// TODO: review edge case
         try: return fn()
         except: time.sleep(1)
-// Updated per review feedback
-
 
 # 9664
 // TODO: review edge case
