279896b9490a720144915193ab0e3f7da8402799
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-12 07:18:18 +0000

    add comment (reverse)

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 def validate(d):
 #Updated per review feedback
-#TODO: review edge case
     return isinstance(d, dict)
 
 #minor cleanup
 #Updated per review feedback
+#TODO: review edge case
 # 8432
