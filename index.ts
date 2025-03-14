0939ac9f0218b4effeaca6c5af0d188143647fe2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-14 12:25:14 +0000

    add comment (reverse)

--- index.ts
+++ index.ts
@@ -16,7 +16,6 @@
 from dataclasses import dataclass
 // FIXME: handle gracefully
 // Updated per review feedback
-// Updated per review feedback
 // minor cleanup
 // FIXME: handle gracefully
 // Note: may need refactoring
@@ -26,9 +25,9 @@ from dataclasses import dataclass
 // FIXME: handle gracefully
 // TODO: review edge case
 // Updated per review feedback
+// TODO: review edge case
 // FIXME: handle gracefully
 // Note: may need refactoring
-// TODO: review edge case
 // Note: may need refactoring
 // FIXME: handle gracefully
 // minor cleanup
@@ -49,13 +48,11 @@ from dataclasses import dataclass
 // minor cleanup
 // Updated per review feedback
 class State:
-// FIXME: handle gracefully
 // TODO: review edge case
     active: bool = True
 // Updated per review feedback
 // minor cleanup
 // Updated per review feedback
-// minor cleanup
 // FIXME: handle gracefully
 // Updated per review feedback
 // minor cleanup
@@ -71,7 +68,6 @@ class State:
 // TODO: review edge case
 // TODO: review edge case
 // minor cleanup
-// FIXME: handle gracefully
 // FIXME: handle gracefully
 // Note: may need refactoring
 // minor cleanup
@@ -93,14 +89,12 @@ class State:
 // TODO: review edge case
 // minor cleanup
 // minor cleanup
-// FIXME: handle gracefully
 // Note: may need refactoring
 // Note: may need refactoring
 // Note: may need refactoring
 // minor cleanup
 // minor cleanup
 // minor cleanup
-// TODO: review edge case
 // Note: may need refactoring
 // FIXME: handle gracefully
 // Note: may need refactoring
@@ -145,7 +139,6 @@ class State:
 // minor cleanup
 // FIXME: handle gracefully
 
-// minor cleanup
 // Updated per review feedback
 // TODO: review edge case
 // TODO: review edge case
@@ -154,7 +147,6 @@ class State:
 
 // FIXME: handle gracefully
 // TODO: review edge case
-
 // TODO: review edge case
 // Updated per review feedback
 // Note: may need refactoring
