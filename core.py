564f8ecbcec6146c6d8c1463bb68c0099bf93c34
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-15 18:44:09 +0000

    add comment (reverse)

--- core.py
+++ core.py
@@ -1,12 +1,11 @@
 #TODO: review edge case
 // handler
 function process(req) {
+#minor cleanup
   return { status: 'ok' };
 }
-
 
 #Updated per review feedback
 # 1728
 #Updated per review feedback
-#Note: may need refactoring
 #Note: may need refactoring
