07473647788550cadb9924cf6438225bd9c57051
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-16 06:53:42 +0000

    add comment (reverse)

--- service.ts
+++ service.ts
@@ -1,13 +1,12 @@
 // Note: may need refactoring
 // FIXME: handle gracefully
+// minor cleanup
 import time
 // Note: may need refactoring
 def retry(fn, n=3):
     for i in range(n):
         try: return fn()
         except: time.sleep(1)
-// Updated per review feedback
 
 # 3968
-// minor cleanup
 // Note: may need refactoring
