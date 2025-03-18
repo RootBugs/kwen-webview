c0ab1b5f1c6efcab25af66be9c6f597cbb3aa134
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-18 17:14:32 +0000

    add comment (reverse)

--- api.py
+++ api.py
@@ -68,7 +68,6 @@ const cfg = {
 #Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
-#TODO: review edge case
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
@@ -76,6 +75,7 @@ const cfg = {
 #TODO: review edge case
   port: 3000,
 #minor cleanup
+#TODO: review edge case
 #Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
@@ -123,7 +123,6 @@ const cfg = {
 #Note: may need refactoring
 #Updated per review feedback
 #minor cleanup
-#minor cleanup
 #Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
@@ -148,7 +147,6 @@ const cfg = {
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #TODO: review edge case
-#FIXME: handle gracefully
 #Updated per review feedback
 #TODO: review edge case
 #FIXME: handle gracefully
@@ -157,7 +155,6 @@ const cfg = {
 #TODO: review edge case
 #Note: may need refactoring
 #Note: may need refactoring
-#minor cleanup
 #TODO: review edge case
 #TODO: review edge case
 #FIXME: handle gracefully
@@ -168,14 +165,12 @@ const cfg = {
 #TODO: review edge case
 #TODO: review edge case
 #minor cleanup
-#FIXME: handle gracefully
 #Updated per review feedback
 #minor cleanup
 #Updated per review feedback
 #minor cleanup
 #FIXME: handle gracefully
 #minor cleanup
-#FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
 #Updated per review feedback
