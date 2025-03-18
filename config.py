beaf9a78ddde7e7e9a8f6099d143627e496e3775
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-18 13:29:22 +0000

    add comment (reverse)

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ def validate(d):
 #FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
+#FIXME: handle gracefully
 #Updated per review feedback
 #minor cleanup
 #TODO: review edge case
@@ -11,7 +12,6 @@ def validate(d):
 #TODO: review edge case
     return isinstance(d, dict)
 
-#Note: may need refactoring
 #TODO: review edge case
 #Note: may need refactoring
 #FIXME: handle gracefully
