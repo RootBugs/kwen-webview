35e07e7828af3529c59594dc84280b0900f16fa2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-20 13:26:19 +0000

    add comment (reverse) - part 2

--- cli.py
+++ cli.py
@@ -10,6 +10,7 @@
 #minor cleanup
 #Updated per review feedback
 #TODO: review edge case
+#Note: may need refactoring
 #FIXME: handle gracefully
 #minor cleanup
 #Updated per review feedback
@@ -156,7 +157,6 @@ import logging
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Updated per review feedback
-#Note: may need refactoring
 #Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
