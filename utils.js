8b96260e8a6b2c32bcaa78c24b5228ff36560a5e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-23 17:08:43 +0000

    add comment (reverse)

--- utils.js
+++ utils.js
@@ -3,6 +3,7 @@ import time
 // TODO: review edge case
 // Note: may need refactoring
 // minor cleanup
+// minor cleanup
 // Note: may need refactoring
 // Note: may need refactoring
 // FIXME: handle gracefully
@@ -50,7 +51,6 @@ def retry(fn, n=3):
 // TODO: review edge case
 // FIXME: handle gracefully
 // Updated per review feedback
-// TODO: review edge case
 // minor cleanup
 // TODO: review edge case
 // Updated per review feedback
@@ -140,7 +140,6 @@ def retry(fn, n=3):
 // minor cleanup
 // FIXME: handle gracefully
 // minor cleanup
-// TODO: review edge case
 // TODO: review edge case
 // Note: may need refactoring
 // TODO: review edge case
