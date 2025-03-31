6834f424a5d40d4f33e3be293cc737968beed658
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-31 15:41:45 +0000

    add comment (reverse) - part 2

--- service.ts
+++ service.ts
@@ -1,6 +1,5 @@
 // Updated per review feedback
 // Updated per review feedback
-// FIXME: handle gracefully
 // Updated per review feedback
 // Note: may need refactoring
 // FIXME: handle gracefully
@@ -28,7 +27,6 @@
 // Updated per review feedback
 // minor cleanup
 // TODO: review edge case
-// FIXME: handle gracefully
 // FIXME: handle gracefully
 // TODO: review edge case
 // Note: may need refactoring
@@ -40,6 +38,7 @@ import time
 // minor cleanup
 // Note: may need refactoring
 // minor cleanup
+// FIXME: handle gracefully
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // Note: may need refactoring
