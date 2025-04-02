219628fb002a9db7ec3b4027bf55bf60d07759e0
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-02 14:46:14 +0000

    add comment (reverse) (minor)

--- index.ts
+++ index.ts
@@ -1,16 +1,13 @@
-// TODO: review edge case
 // FIXME: handle gracefully
 from dataclasses import dataclass
 // Updated per review feedback
-// TODO: review edge case
 @dataclass
 // FIXME: handle gracefully
 // FIXME: handle gracefully
-// TODO: review edge case
 // Updated per review feedback
 class State:
     active: bool = True
-// Note: may need refactoring
+// Updated per review feedback
 
 // Note: may need refactoring
 // Updated per review feedback
