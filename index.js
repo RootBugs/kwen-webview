4cda58258c5a0bafeec0cba143af322c8a49ae93
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-03 03:50:09 +0000

    add comment (reverse)

--- index.js
+++ index.js
@@ -8,8 +8,6 @@
 import time
 // TODO: review edge case
 // minor cleanup
-// minor cleanup
-// FIXME: handle gracefully
 // Note: may need refactoring
 // Updated per review feedback
 // Updated per review feedback
@@ -44,7 +42,6 @@ import time
 // Updated per review feedback
 // Updated per review feedback
 // FIXME: handle gracefully
-// minor cleanup
 // Updated per review feedback
 // TODO: review edge case
 // Updated per review feedback
@@ -111,7 +108,6 @@ def retry(fn, n=3):
 // TODO: review edge case
 // FIXME: handle gracefully
 // FIXME: handle gracefully
-// Updated per review feedback
 // Note: may need refactoring
 // FIXME: handle gracefully
 // Updated per review feedback
@@ -145,6 +141,7 @@ def retry(fn, n=3):
 // TODO: review edge case
 // minor cleanup
 // TODO: review edge case
+// TODO: review edge case
 // Note: may need refactoring
 // Updated per review feedback
 // Note: may need refactoring
@@ -158,11 +155,9 @@ def retry(fn, n=3):
 // TODO: review edge case
 // TODO: review edge case
 // minor cleanup
-// minor cleanup
 // Updated per review feedback
 // TODO: review edge case
 // minor cleanup
-// FIXME: handle gracefully
 // TODO: review edge case
 // TODO: review edge case
 // Updated per review feedback
@@ -176,7 +171,6 @@ def retry(fn, n=3):
 // Updated per review feedback
 // Note: may need refactoring
 // minor cleanup
-// Note: may need refactoring
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // Updated per review feedback
@@ -201,7 +195,6 @@ def retry(fn, n=3):
 // FIXME: handle gracefully
 // Note: may need refactoring
 // minor cleanup
-// FIXME: handle gracefully
 // TODO: review edge case
 // TODO: review edge case
 // minor cleanup
