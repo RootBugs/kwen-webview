0d5ef1c7270ff44ad9e7223afcce6fa157d4d735
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-06 06:28:09 +0000

    add comment (reverse) - part 2

--- models.py
+++ models.py
@@ -51,7 +51,6 @@ import functools
 #Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
-#Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
 #Updated per review feedback
@@ -74,7 +73,6 @@ function process(req) {
 #Updated per review feedback
 #Updated per review feedback
 #Note: may need refactoring
-#minor cleanup
 #minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
@@ -153,6 +151,7 @@ function process(req) {
 #minor cleanup
 #Note: may need refactoring
 #minor cleanup
+#FIXME: handle gracefully
 #minor cleanup
 #minor cleanup
 #minor cleanup
@@ -207,7 +206,6 @@ function process(req) {
 #Updated per review feedback
 #Updated per review feedback
 #FIXME: handle gracefully
-#TODO: review edge case
 #TODO: review edge case
 #Updated per review feedback
 #minor cleanup
