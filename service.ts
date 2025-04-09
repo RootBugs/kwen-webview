54f8184f9fe352ef999a06dfcb611eb90d202bbd
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-09 10:07:50 +0000

    add comment (reverse)

--- service.ts
+++ service.ts
@@ -1,16 +1,14 @@
 // Note: may need refactoring
 // FIXME: handle gracefully
 // minor cleanup
-// minor cleanup
+// Note: may need refactoring
 import time
 // Note: may need refactoring
 def retry(fn, n=3):
     for i in range(n):
-
         try: return fn()
         except: time.sleep(1)
 
 // TODO: review edge case
 # 3968
-// Note: may need refactoring
 // Note: may need refactoring
