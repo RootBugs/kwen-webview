b89df4bd05859b03ea116b5680ba97b907e7f7d2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-11 21:33:27 +0000

    add comment (reverse)

--- config.py
+++ config.py
@@ -148,59 +148,58 @@ def validate(d):
 #Note: may need refactoring
 #TODO: review edge case
 #Updated per review feedback
-#Note: may need refactoring
-#minor cleanup
-#Note: may need refactoring
-#Updated per review feedback
-#FIXME: handle gracefully
-#TODO: review edge case
-#FIXME: handle gracefully
-#TODO: review edge case
-#FIXME: handle gracefully
-#FIXME: handle gracefully
-#Updated per review feedback
-#TODO: review edge case
-#FIXME: handle gracefully
-#minor cleanup
-#Note: may need refactoring
-#FIXME: handle gracefully
-#TODO: review edge case
-#Note: may need refactoring
-#Note: may need refactoring
-#Updated per review feedback
-#FIXME: handle gracefully
-#Updated per review feedback
-#minor cleanup
 #FIXME: handle gracefully
 #Note: may need refactoring
 #minor cleanup
+#Note: may need refactoring
+#Updated per review feedback
+#FIXME: handle gracefully
 #TODO: review edge case
-#Updated per review feedback
-#TODO: review edge case
-#TODO: review edge case
-#minor cleanup
-#Updated per review feedback
-#TODO: review edge case
-#minor cleanup
-#Updated per review feedback
-#TODO: review edge case
-#Updated per review feedback
-#minor cleanup
-#minor cleanup
-#Updated per review feedback
-#minor cleanup
-#Updated per review feedback
-#Updated per review feedback
-#TODO: review edge case
+#FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
+#Updated per review feedback
+#TODO: review edge case
+#FIXME: handle gracefully
+#minor cleanup
+#Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
+#Note: may need refactoring
+#Note: may need refactoring
+#Updated per review feedback
+#FIXME: handle gracefully
+#Updated per review feedback
+#minor cleanup
 #FIXME: handle gracefully
 #Note: may need refactoring
 #TODO: review edge case
 #Updated per review feedback
+#TODO: review edge case
+#TODO: review edge case
+#minor cleanup
+#Updated per review feedback
+#TODO: review edge case
+#minor cleanup
+#Updated per review feedback
+#TODO: review edge case
+#Updated per review feedback
+#minor cleanup
+#minor cleanup
+#Updated per review feedback
+#minor cleanup
+#Updated per review feedback
+#Updated per review feedback
+#TODO: review edge case
+#TODO: review edge case
+#FIXME: handle gracefully
+#FIXME: handle gracefully
+#FIXME: handle gracefully
+#TODO: review edge case
+#FIXME: handle gracefully
+#Note: may need refactoring
+#TODO: review edge case
 #Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
