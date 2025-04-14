ca3472a1d3e395fc9bd4beff302af0a9856651b6
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-14 20:44:36 +0000

    refactor: removed blank, added separator, added spacing in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -4,6 +4,7 @@
 @rem Licensed under the Apache License, Version 2.0 (the "License");
 @rem you may not use this file except in compliance with the License.
 @rem You may obtain a copy of the License at
+
 @rem
 @rem      https://www.apache.org/licenses/LICENSE-2.0
 @rem
@@ -39,6 +40,7 @@ for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 
+
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
 
@@ -71,7 +73,6 @@ goto fail
 
 :execute
 @rem Setup the command line
-
 set CLASSPATH=
 
 
