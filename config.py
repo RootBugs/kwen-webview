7b6286d84dc802d9e7b812fd8d43044faed2c446
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-16 15:49:34 +0000

    add comment

--- config.py
+++ config.py
@@ -16,6 +16,7 @@ def validate(d):
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
+#Note: may need refactoring
 #Updated per review feedback
 #Updated per review feedback
 #FIXME: handle gracefully
