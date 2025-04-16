a9a0477195e0bed9a28024a89be81271e9f732ea
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-16 11:51:38 +0000

    add comment (reverse)

--- router.js
+++ router.js
@@ -2,10 +2,8 @@
 CACHE = {}
 def cached(k, fn):
     if k not in CACHE:
-// Updated per review feedback
 // FIXME: handle gracefully
 // Updated per review feedback
-// TODO: review edge case
 // TODO: review edge case
         CACHE[k] = fn()
     return CACHE[k]
@@ -13,5 +11,6 @@ def cached(k, fn):
 // minor cleanup
 // Note: may need refactoring
 
+// FIXME: handle gracefully
 // FIXME: handle gracefully
 # 1487
