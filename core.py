0b468352d7493a8dcc87e840bd5be4c6003e0db0
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-18 14:20:57 +0000

    add comment (reverse) (minor)

--- core.py
+++ core.py
@@ -23,6 +23,7 @@ import pathlib
 #Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
+#Updated per review feedback
 #Note: may need refactoring
 #TODO: review edge case
 #FIXME: handle gracefully
@@ -61,7 +62,6 @@ function process(req) {
 #minor cleanup
 #TODO: review edge case
 #TODO: review edge case
-#FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
 #Note: may need refactoring
@@ -108,7 +108,6 @@ function process(req) {
 #FIXME: handle gracefully
 #Updated per review feedback
 #minor cleanup
-#FIXME: handle gracefully
 #Note: may need refactoring
 #TODO: review edge case
 #Note: may need refactoring
@@ -132,7 +131,6 @@ function process(req) {
 #TODO: review edge case
 #Note: may need refactoring
 #minor cleanup
-#Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
