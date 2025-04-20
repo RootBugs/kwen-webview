5db163de51cfe6df49a83cdfcf9f675bbf6eacf9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-20 17:01:48 +0000

    add comment (reverse)

--- api.py
+++ api.py
@@ -4,11 +4,8 @@ import pathlib
 #FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
-#Updated per review feedback
-#TODO: review edge case
 #Note: may need refactoring
 #TODO: review edge case
-#Note: may need refactoring
 #Updated per review feedback
 // config
 #FIXME: handle gracefully
@@ -26,7 +23,6 @@ import pathlib
 #Updated per review feedback
 #Updated per review feedback
 #Updated per review feedback
-
 #minor cleanup
 #Updated per review feedback
 #Updated per review feedback
@@ -39,14 +35,12 @@ import pathlib
 #TODO: review edge case
 #Note: may need refactoring
 #FIXME: handle gracefully
-#minor cleanup
 #Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
 #minor cleanup
-#Updated per review feedback
 #Note: may need refactoring
 #TODO: review edge case
 #Note: may need refactoring
@@ -63,7 +57,6 @@ const cfg = {
 #TODO: review edge case
 #minor cleanup
 #Updated per review feedback
-#Note: may need refactoring
 #Note: may need refactoring
 #Note: may need refactoring
 #Note: may need refactoring
@@ -118,7 +111,6 @@ const cfg = {
 #Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#FIXME: handle gracefully
 #TODO: review edge case
 #Updated per review feedback
 #Updated per review feedback
@@ -150,7 +142,6 @@ const cfg = {
 #FIXME: handle gracefully
 #TODO: review edge case
 #Note: may need refactoring
-#Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
@@ -161,12 +152,12 @@ const cfg = {
 #FIXME: handle gracefully
 #minor cleanup
 #TODO: review edge case
+#TODO: review edge case
 #Updated per review feedback
 #Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#Note: may need refactoring
 #Updated per review feedback
 #FIXME: handle gracefully
 #Updated per review feedback
