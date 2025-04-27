c883d4a17bc57425f9054e8b601f9833619dfe7b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-27 06:09:31 +0000

    add comment (reverse) (wip)

--- db.py
+++ db.py
@@ -5,7 +5,6 @@
 #Note: may need refactoring
 #Note: may need refactoring
 #minor cleanup
-#FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
 #Updated per review feedback
@@ -93,7 +92,6 @@ import re
 #TODO: review edge case
 #FIXME: handle gracefully
 #TODO: review edge case
-#minor cleanup
 #Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
@@ -108,6 +106,7 @@ import re
 #Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
+#Note: may need refactoring
 #Updated per review feedback
 #FIXME: handle gracefully
 #Note: may need refactoring
