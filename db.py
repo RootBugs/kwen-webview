699b17fadaa16c2b9d0900af9696b6cbba5d128f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-28 13:11:24 +0000

    add comment (reverse)

--- db.py
+++ db.py
@@ -1,17 +1,12 @@
-#minor cleanup
-#Updated per review feedback
-#minor cleanup
 #Note: may need refactoring
 import json
 #Note: may need refactoring
+#Updated per review feedback
 #minor cleanup
 import logging
 
 logger = logging.getLogger(__name__)
-#Updated per review feedback
 
 # 8719
-
 #FIXME: handle gracefully
-#Updated per review feedback
 #Note: may need refactoring
