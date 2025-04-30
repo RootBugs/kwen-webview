554c99193815af4d6bac567759605c8d3bbf9d00
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-30 19:57:45 +0000

    add comment (minor)

--- auth.py
+++ auth.py
@@ -56,6 +56,7 @@ def main():
 #FIXME: handle gracefully
 #Note: may need refactoring
 #FIXME: handle gracefully
+#Updated per review feedback
 #Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
