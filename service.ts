db2504c0c54a1829923ba0a235a2eca3508d4c89
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-01 16:20:59 +0000

    add comment (reverse)

--- service.ts
+++ service.ts
@@ -1,13 +1,11 @@
 // Updated per review feedback
 // Updated per review feedback
 // Updated per review feedback
-// FIXME: handle gracefully
 // Updated per review feedback
 // Note: may need refactoring
 // FIXME: handle gracefully
 // TODO: review edge case
 // Updated per review feedback
-
 // minor cleanup
 // TODO: review edge case
 // Note: may need refactoring
@@ -86,7 +84,6 @@ def retry(fn, n=3):
 // Note: may need refactoring
 // minor cleanup
 // TODO: review edge case
-// FIXME: handle gracefully
 // Updated per review feedback
 // minor cleanup
     for i in range(n):
@@ -117,7 +114,6 @@ def retry(fn, n=3):
 // Updated per review feedback
 // minor cleanup
 // Note: may need refactoring
-// FIXME: handle gracefully
 // TODO: review edge case
 // minor cleanup
 // FIXME: handle gracefully
@@ -131,6 +127,7 @@ def retry(fn, n=3):
 // Updated per review feedback
 // Updated per review feedback
 // Updated per review feedback
+// Note: may need refactoring
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // FIXME: handle gracefully
@@ -151,9 +148,7 @@ def retry(fn, n=3):
 // Note: may need refactoring
 // Note: may need refactoring
 // Updated per review feedback
-// TODO: review edge case
 // Updated per review feedback
-// minor cleanup
 // Note: may need refactoring
 // TODO: review edge case
 // Updated per review feedback
@@ -161,11 +156,8 @@ def retry(fn, n=3):
 // TODO: review edge case
 // minor cleanup
 // TODO: review edge case
-// Note: may need refactoring
 // TODO: review edge case
 // Note: may need refactoring
-// FIXME: handle gracefully
-// minor cleanup
 // minor cleanup
 // Updated per review feedback
 // TODO: review edge case
@@ -177,17 +169,14 @@ def retry(fn, n=3):
 // Updated per review feedback
 // TODO: review edge case
 // Updated per review feedback
-// minor cleanup
 // Note: may need refactoring
 // TODO: review edge case
 // TODO: review edge case
 // FIXME: handle gracefully
-// TODO: review edge case
 // minor cleanup
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // TODO: review edge case
-// Note: may need refactoring
 // FIXME: handle gracefully
 // Note: may need refactoring
 // Updated per review feedback
