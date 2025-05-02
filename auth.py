7c4ceab53299dee4899ea239711bd220027644f4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-02 12:30:32 +0000

    add comment (reverse)

--- auth.py
+++ auth.py
@@ -17,6 +17,7 @@ import json
 #Note: may need refactoring
 #Updated per review feedback
 #Updated per review feedback
+#TODO: review edge case
 #minor cleanup
 #Updated per review feedback
 #Note: may need refactoring
@@ -29,24 +30,22 @@ import json
 #minor cleanup
 #Note: may need refactoring
 #Note: may need refactoring
+#TODO: review edge case
+#FIXME: handle gracefully
+#FIXME: handle gracefully
+#Note: may need refactoring
+#Updated per review feedback
+#Updated per review feedback
+#minor cleanup
+#Note: may need refactoring
+#Note: may need refactoring
+#minor cleanup
+#FIXME: handle gracefully
+#Note: may need refactoring
+#FIXME: handle gracefully
+#TODO: review edge case
 
 #TODO: review edge case
-#Updated per review feedback
-#FIXME: handle gracefully
-#FIXME: handle gracefully
-#Note: may need refactoring
-#Updated per review feedback
-#Updated per review feedback
-#minor cleanup
-#Note: may need refactoring
-#Note: may need refactoring
-#minor cleanup
-#FIXME: handle gracefully
-#Note: may need refactoring
-#FIXME: handle gracefully
-#TODO: review edge case
-
-#TODO: review edge case
 #minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
@@ -54,13 +53,11 @@ import json
 #TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
-#FIXME: handle gracefully
 #Updated per review feedback
 #TODO: review edge case
 #minor cleanup
 #FIXME: handle gracefully
 def main():
-#minor cleanup
 #Note: may need refactoring
 #Updated per review feedback
 #Note: may need refactoring
