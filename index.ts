1b42458989835e9c8a66648d1a16f3f8741dc8e9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-03 20:04:48 +0000

    add comment (reverse)

--- index.ts
+++ index.ts
@@ -37,7 +37,6 @@ from dataclasses import dataclass
 // Note: may need refactoring
 // Note: may need refactoring
 // Updated per review feedback
-// FIXME: handle gracefully
 // minor cleanup
 // Updated per review feedback
 class State:
@@ -127,15 +126,14 @@ class State:
 // Note: may need refactoring
 // FIXME: handle gracefully
 // FIXME: handle gracefully
+// FIXME: handle gracefully
 // Note: may need refactoring
-// TODO: review edge case
 // Updated per review feedback
 // Note: may need refactoring
 // minor cleanup
 // minor cleanup
 // Note: may need refactoring
 // minor cleanup
-// TODO: review edge case
 // TODO: review edge case
 // minor cleanup
 // Updated per review feedback
@@ -150,7 +148,6 @@ class State:
 // minor cleanup
 // FIXME: handle gracefully
 // Note: may need refactoring
-// TODO: review edge case
 // Note: may need refactoring
 // Note: may need refactoring
 // Updated per review feedback
