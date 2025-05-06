c6f7601255e9f54069fe8df7d8f7073adefd14cb
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-06 03:42:17 +0000

    add comment (minor)

--- config.py
+++ config.py
@@ -140,6 +140,7 @@ def validate(d):
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Updated per review feedback
+#FIXME: handle gracefully
 #Note: may need refactoring
 #minor cleanup
 #TODO: review edge case
