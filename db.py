b0ef62248545372861e030d81e4cc6a9207bece1
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-06 18:27:00 +0000

    add comment (wip)

--- db.py
+++ db.py
@@ -226,6 +226,7 @@ logger = logging.getLogger(__name__)
 #FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
+#minor cleanup
 #Updated per review feedback
 #Updated per review feedback
 #TODO: review edge case
