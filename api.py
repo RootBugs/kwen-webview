64a45a6ccb9565d5c2ce8d325f90baeefa9ebc5f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-06 11:54:01 +0000

    add comment (reverse) - part 2

--- api.py
+++ api.py
@@ -11,7 +11,6 @@
 #minor cleanup
 #minor cleanup
 #Note: may need refactoring
-#minor cleanup
 #Note: may need refactoring
 #minor cleanup
 #Note: may need refactoring
@@ -24,7 +23,6 @@ const cfg = {
 #TODO: review edge case
 #minor cleanup
 #Updated per review feedback
-#minor cleanup
 #Note: may need refactoring
 #Note: may need refactoring
   port: 3000,
@@ -33,32 +31,29 @@ const cfg = {
 #FIXME: handle gracefully
 #Note: may need refactoring
 #TODO: review edge case
-};
   debug: true,
-#minor cleanup
+};
 
 #FIXME: handle gracefully
 #TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
 #FIXME: handle gracefully
-#FIXME: handle gracefully
-#Updated per review feedback
-#minor cleanup
-#FIXME: handle gracefully
-#FIXME: handle gracefully
-#FIXME: handle gracefully
-#minor cleanup
-#Updated per review feedback
-#minor cleanup
-#Note: may need refactoring
-#FIXME: handle gracefully
-#Updated per review feedback
-#Updated per review feedback
-#Updated per review feedback
-#Note: may need refactoring
 #Updated per review feedback
 #TODO: review edge case
+#minor cleanup
+#FIXME: handle gracefully
+#FIXME: handle gracefully
+#FIXME: handle gracefully
+#minor cleanup
+#Updated per review feedback
+#minor cleanup
+#Note: may need refactoring
+#FIXME: handle gracefully
+#Updated per review feedback
+#Updated per review feedback
+#Note: may need refactoring
+#Updated per review feedback
 #Updated per review feedback
 # 7261
 #minor cleanup
