83966647dcafc3607d0dcac046597a8371731e02
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-11 15:07:47 +0000

    add comment (reverse)

--- core.py
+++ core.py
@@ -1,6 +1,5 @@
 #Note: may need refactoring
 import functools
-#FIXME: handle gracefully
 import random
 import json
 #minor cleanup
@@ -151,6 +150,7 @@ function process(req) {
 #Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
+#Note: may need refactoring
 #minor cleanup
 #minor cleanup
 #minor cleanup
