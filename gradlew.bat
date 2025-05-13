5c9b75c773394011892988697480b303bacaa38a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-13 20:14:47 +0000

    update: added separator, added spacing in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -30,6 +30,7 @@ if "%OS%"=="Windows_NT" setlocal
 set DIRNAME=%~dp0
 if "%DIRNAME%"=="" set DIRNAME=.
 @rem This is normally unused
+
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
@@ -77,6 +78,7 @@ set CLASSPATH=
 
 @rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
+
 
 :end
 @rem End local scope for the variables with windows NT shell
