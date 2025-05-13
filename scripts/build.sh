1d38c65b0c7c71bd57844b7cb522ceea6b0b055b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-13 10:26:50 +0000

    clean: removed blank, removed blank, removed blank in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,10 @@
 # revised: logic  # HACK: refactor
 
 
-
-
 set -e  # TODO: performance  # optimize: performance  # HACK: cleanup
 echo "Building project..."
 
 npm run build
-
 
 
 echo "Build complete!"
