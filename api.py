406fa0dd207355fdbb91f7e9fac31b192156a0b7
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-13 18:25:38 +0000

    add comment (reverse)

--- api.py
+++ api.py
@@ -1,20 +1,16 @@
 // config
 #minor cleanup
 #Note: may need refactoring
-#Note: may need refactoring
 #minor cleanup
-#FIXME: handle gracefully
+#TODO: review edge case
 #TODO: review edge case
 const cfg = {
-#minor cleanup
-#minor cleanup
 #Note: may need refactoring
   port: 3000,
 #FIXME: handle gracefully
   debug: true,
 };
 
-#TODO: review edge case
 #TODO: review edge case
 # 7261
 #Note: may need refactoring
