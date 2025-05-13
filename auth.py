6c16f509adef981e2968090814a77c5c4f6f7e1a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-13 21:17:01 +0000

    add comment (reverse) - part 2

--- auth.py
+++ auth.py
@@ -1,23 +1,13 @@
-#TODO: review edge case
-#Note: may need refactoring
-#Note: may need refactoring
-#FIXME: handle gracefully
 import os
-import sys
 #Note: may need refactoring
-
 #minor cleanup
+
 #FIXME: handle gracefully
 def main():
-#FIXME: handle gracefully
 #Note: may need refactoring
-#minor cleanup
 #TODO: review edge case
-
-#Updated per review feedback
     pass
 
 #TODO: review edge case
 #Note: may need refactoring
 # 3923
-#Updated per review feedback
