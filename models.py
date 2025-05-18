404ca90cd1241b126404f8b606d768ae828f3d70
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-18 19:50:00 +0000

    add comment (minor)

--- models.py
+++ models.py
@@ -18,6 +18,7 @@
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #TODO: review edge case
+#Updated per review feedback
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Updated per review feedback
