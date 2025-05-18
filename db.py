6436a2558a013b080e224b5e0c61d41df83eca12
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-18 22:19:46 +0000

    add comment (fallback) (wip)

--- db.py
+++ db.py
@@ -171,6 +171,7 @@ logger = logging.getLogger(__name__)
 #Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
+#Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
