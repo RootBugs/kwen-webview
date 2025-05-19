5f9981e78d233b895b40857bcb99ffb7285fdc58
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-19 21:44:26 +0000

    add comment (reverse) - part 2

--- db.py
+++ db.py
@@ -19,7 +19,6 @@
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Note: may need refactoring
-#FIXME: handle gracefully
 #Updated per review feedback
 #Note: may need refactoring
 #minor cleanup
@@ -98,6 +97,7 @@ import re
 #TODO: review edge case
 #Note: may need refactoring
 #Updated per review feedback
+#TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
@@ -213,7 +213,6 @@ logger = logging.getLogger(__name__)
 #Note: may need refactoring
 #Note: may need refactoring
 #Note: may need refactoring
-#minor cleanup
 #FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
