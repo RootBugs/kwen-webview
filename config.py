b523df17ec491d056301d9257b5173e10bd73d55
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-19 15:36:54 +0000

    add comment (reverse)

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import collections
-import random
 #TODO: review edge case
 #Note: may need refactoring
 #Updated per review feedback
@@ -49,8 +48,8 @@ def validate(d):
 
 #Note: may need refactoring
 #TODO: review edge case
+#Note: may need refactoring
 #TODO: review edge case
-#Updated per review feedback
 #Updated per review feedback
 #Updated per review feedback
 #Note: may need refactoring
@@ -64,7 +63,6 @@ def validate(d):
 #minor cleanup
 #Updated per review feedback
 #Updated per review feedback
-#Note: may need refactoring
 #TODO: review edge case
 #Updated per review feedback
 #TODO: review edge case
@@ -99,14 +97,12 @@ def validate(d):
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
-#TODO: review edge case
 #FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
 #minor cleanup
 #FIXME: handle gracefully
 #TODO: review edge case
-#minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Note: may need refactoring
@@ -142,7 +138,6 @@ def validate(d):
 #Note: may need refactoring
 #Updated per review feedback
 #Updated per review feedback
-#Note: may need refactoring
 #Note: may need refactoring
 #TODO: review edge case
 #Updated per review feedback
