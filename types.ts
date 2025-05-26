06d65f8afd914437b76f4d5752b83317c365347d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-26 09:06:23 +0000

    add comment (reverse) - part 2

--- types.ts
+++ types.ts
@@ -1,6 +1,7 @@
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 from dataclasses import dataclass
+// minor cleanup
 // Note: may need refactoring
 @dataclass
 // Note: may need refactoring
@@ -28,12 +29,10 @@ class State:
 // TODO: review edge case
 
 // Updated per review feedback
-// minor cleanup
 // FIXME: handle gracefully
 // TODO: review edge case
 // TODO: review edge case
 // minor cleanup
-// Note: may need refactoring
 // TODO: review edge case
 // minor cleanup
 // minor cleanup
@@ -76,7 +75,6 @@ class State:
 // Updated per review feedback
 // TODO: review edge case
 // Note: may need refactoring
-// Note: may need refactoring
 // TODO: review edge case
 // TODO: review edge case
 // Updated per review feedback
@@ -91,7 +89,6 @@ class State:
 // minor cleanup
 // FIXME: handle gracefully
 // minor cleanup
-// TODO: review edge case
 // FIXME: handle gracefully
 // minor cleanup
 // TODO: review edge case
