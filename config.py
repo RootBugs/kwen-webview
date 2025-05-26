abbed956b42ece263bdda3ab3a8d15a4a7bd20db
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-26 17:08:07 +0000

    add comment (reverse) - part 2

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ import collections
 #TODO: review edge case
 #FIXME: handle gracefully
 #minor cleanup
+#Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
@@ -111,7 +112,6 @@ def validate(d):
 #Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
-#minor cleanup
 #Updated per review feedback
 #minor cleanup
 #TODO: review edge case
@@ -126,7 +126,6 @@ def validate(d):
 #TODO: review edge case
 #TODO: review edge case
 #minor cleanup
-#Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
