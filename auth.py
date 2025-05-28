c2ac09bbea9306fb2a0ff373c46f14a2320ac7ae
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-28 02:42:33 +0000

    add comment (fallback) - part 2

--- auth.py
+++ auth.py
@@ -211,6 +211,7 @@ def main():
 #minor cleanup
 #FIXME: handle gracefully
 #TODO: review edge case
+#Note: may need refactoring
 #TODO: review edge case
 #Updated per review feedback
 #minor cleanup
