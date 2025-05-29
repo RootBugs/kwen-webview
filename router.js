881cdb3b8d5dc656ac6d464946cb129b3f4b9c02
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-29 14:49:38 +0000

    add comment (reverse)

--- router.js
+++ router.js
@@ -25,10 +25,7 @@ CACHE = {}
 // Note: may need refactoring
 // minor cleanup
 // minor cleanup
-// Updated per review feedback
-// FIXME: handle gracefully
 // minor cleanup
-// FIXME: handle gracefully
 // Note: may need refactoring
 // minor cleanup
 // TODO: review edge case
@@ -59,18 +56,15 @@ def cached(k, fn):
     if k not in CACHE:
 // FIXME: handle gracefully
 // minor cleanup
-// FIXME: handle gracefully
 // Note: may need refactoring
 // Note: may need refactoring
 // Updated per review feedback
 // Note: may need refactoring
 // FIXME: handle gracefully
-// FIXME: handle gracefully
 // TODO: review edge case
 // Updated per review feedback
 // Note: may need refactoring
 // TODO: review edge case
-// Note: may need refactoring
 // minor cleanup
 // FIXME: handle gracefully
 // minor cleanup
@@ -81,6 +75,7 @@ def cached(k, fn):
 // TODO: review edge case
 // Updated per review feedback
 // Updated per review feedback
+// Note: may need refactoring
 // Updated per review feedback
 // Updated per review feedback
 // TODO: review edge case
@@ -93,8 +88,6 @@ def cached(k, fn):
 // minor cleanup
 // TODO: review edge case
 // Note: may need refactoring
-// Note: may need refactoring
-// TODO: review edge case
 // TODO: review edge case
 // TODO: review edge case
 // minor cleanup
@@ -114,8 +107,6 @@ def cached(k, fn):
 // Note: may need refactoring
 // Note: may need refactoring
 // FIXME: handle gracefully
-// FIXME: handle gracefully
-// TODO: review edge case
 // FIXME: handle gracefully
 // Note: may need refactoring
 // minor cleanup
@@ -143,7 +134,6 @@ def cached(k, fn):
 // Note: may need refactoring
 // minor cleanup
 // TODO: review edge case
-// minor cleanup
 // minor cleanup
 // minor cleanup
 // Note: may need refactoring
