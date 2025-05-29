9fd81308c167f180fc2075a6090890512044d2cc
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-29 16:27:12 +0000

    add comment (reverse) - part 2

--- config.py
+++ config.py
@@ -12,6 +12,7 @@ def validate(d):
 #minor cleanup
 #Updated per review feedback
 #FIXME: handle gracefully
+#Updated per review feedback
 #minor cleanup
 #minor cleanup
 #FIXME: handle gracefully
@@ -101,7 +102,6 @@ def validate(d):
 #TODO: review edge case
 #TODO: review edge case
 #minor cleanup
-#minor cleanup
 #FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
@@ -185,7 +185,6 @@ def validate(d):
 #TODO: review edge case
 #Updated per review feedback
 #minor cleanup
-#Note: may need refactoring
 #Note: may need refactoring
 #Note: may need refactoring
 #Note: may need refactoring
