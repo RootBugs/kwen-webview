daa4cf6a19de9ab576e8dac475c6ecea9a7d50c4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-30 03:24:27 +0000

    add comment (minor)

--- auth.py
+++ auth.py
@@ -15,6 +15,7 @@ import os
 #Note: may need refactoring
 #minor cleanup
 #Note: may need refactoring
+#FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
 #Updated per review feedback
