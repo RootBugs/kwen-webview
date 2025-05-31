396f1f12037cd8f5e57e855845dcfbc4e9d095d7
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-31 10:53:59 +0000

    add comment (reverse) - part 2

--- index.ts
+++ index.ts
@@ -31,6 +31,7 @@ class State:
 // Note: may need refactoring
 // Note: may need refactoring
 // Note: may need refactoring
+// Note: may need refactoring
 // FIXME: handle gracefully
 // Note: may need refactoring
 // minor cleanup
@@ -42,7 +43,6 @@ class State:
 
 // TODO: review edge case
 // Updated per review feedback
-// FIXME: handle gracefully
 // Note: may need refactoring
 // FIXME: handle gracefully
 // Note: may need refactoring
