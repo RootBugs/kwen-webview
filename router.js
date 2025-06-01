14ffaa60d9666ed5b3340eb7a49e68ca8cbd4581
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-01 16:23:06 +0000

    add comment (reverse)

--- router.js
+++ router.js
@@ -1,7 +1,6 @@
 // FIXME: handle gracefully
 // Updated per review feedback
 // TODO: review edge case
-
 // FIXME: handle gracefully
 // minor cleanup
 // Updated per review feedback
@@ -92,67 +91,66 @@ def cached(k, fn):
     if k not in CACHE:
 // FIXME: handle gracefully
 // FIXME: handle gracefully
-// FIXME: handle gracefully
-// Updated per review feedback
-// minor cleanup
-// Note: may need refactoring
-// Note: may need refactoring
-// Note: may need refactoring
 // Updated per review feedback
 // minor cleanup
 // Updated per review feedback
 // Note: may need refactoring
 // Note: may need refactoring
 // Note: may need refactoring
-// FIXME: handle gracefully
-// TODO: review edge case
 // Updated per review feedback
-// Note: may need refactoring
-// Note: may need refactoring
-// Note: may need refactoring
-// Note: may need refactoring
-// TODO: review edge case
-// minor cleanup
-// FIXME: handle gracefully
 // minor cleanup
 // Updated per review feedback
-// FIXME: handle gracefully
-// Updated per review feedback
-// TODO: review edge case
 // Note: may need refactoring
-// Updated per review feedback
-// TODO: review edge case
 // Note: may need refactoring
-// minor cleanup
-// TODO: review edge case
-// minor cleanup
+// Note: may need refactoring
 // FIXME: handle gracefully
 // TODO: review edge case
 // Updated per review feedback
-// TODO: review edge case
-// minor cleanup
-// TODO: review edge case
-// Updated per review feedback
-// Updated per review feedback
 // Note: may need refactoring
-// Updated per review feedback
-// Updated per review feedback
-// TODO: review edge case
-// minor cleanup
 // Note: may need refactoring
-// minor cleanup
+// Note: may need refactoring
+// Note: may need refactoring
 // TODO: review edge case
-// Updated per review feedback
+// minor cleanup
+// FIXME: handle gracefully
+// minor cleanup
 // Updated per review feedback
 // FIXME: handle gracefully
 // Updated per review feedback
-// minor cleanup
+// TODO: review edge case
+// Note: may need refactoring
+// Updated per review feedback
+// TODO: review edge case
+// Note: may need refactoring
 // minor cleanup
 // TODO: review edge case
 // minor cleanup
 // FIXME: handle gracefully
+// TODO: review edge case
+// Updated per review feedback
+// TODO: review edge case
+// minor cleanup
+// TODO: review edge case
+// Updated per review feedback
+// Updated per review feedback
 // Note: may need refactoring
-
+// Updated per review feedback
+// Updated per review feedback
+// TODO: review edge case
+// minor cleanup
+// Note: may need refactoring
+// minor cleanup
+// TODO: review edge case
+// Updated per review feedback
+// Updated per review feedback
+// FIXME: handle gracefully
+// Updated per review feedback
+// minor cleanup
+// minor cleanup
+// TODO: review edge case
+// minor cleanup
+// FIXME: handle gracefully
+// Note: may need refactoring
 // minor cleanup
 // minor cleanup
 // TODO: review edge case
@@ -191,7 +189,6 @@ def cached(k, fn):
 // Note: may need refactoring
 // minor cleanup
 // Note: may need refactoring
-// TODO: review edge case
 // Updated per review feedback
 // Note: may need refactoring
     return CACHE[k]
