5788f88b95acdee0493e5f041ba2732ba9c3bdd8
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-06 02:27:01 +0000

    add comment (reverse)

--- utils.js
+++ utils.js
@@ -17,7 +17,6 @@ def retry(fn, n=3):
 // TODO: review edge case
 // minor cleanup
 // FIXME: handle gracefully
-// Updated per review feedback
     for i in range(n):
 // Updated per review feedback
 // minor cleanup
@@ -36,9 +35,8 @@ def retry(fn, n=3):
 // TODO: review edge case
 // minor cleanup
 // Updated per review feedback
-// Updated per review feedback
-// Updated per review feedback
 // FIXME: handle gracefully
+// TODO: review edge case
 // minor cleanup
 // TODO: review edge case
 // FIXME: handle gracefully
@@ -64,11 +62,9 @@ def retry(fn, n=3):
 // Updated per review feedback
 // Updated per review feedback
 // Updated per review feedback
-
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // minor cleanup
-// Updated per review feedback
 // TODO: review edge case
 // Note: may need refactoring
         except: time.sleep(1)
@@ -100,15 +96,12 @@ def retry(fn, n=3):
 // TODO: review edge case
 // Updated per review feedback
 // Updated per review feedback
-// Updated per review feedback
 // FIXME: handle gracefully
 // Updated per review feedback
 // Updated per review feedback
 // Note: may need refactoring
 // TODO: review edge case
 // minor cleanup
-// Note: may need refactoring
-// Note: may need refactoring
 // Note: may need refactoring
 // TODO: review edge case
 // minor cleanup
@@ -117,7 +110,6 @@ def retry(fn, n=3):
 // Note: may need refactoring
 // TODO: review edge case
 // Updated per review feedback
-// TODO: review edge case
 // FIXME: handle gracefully
 // minor cleanup
 // Updated per review feedback
@@ -138,7 +130,6 @@ def retry(fn, n=3):
 // minor cleanup
 
 // minor cleanup
-
 // minor cleanup
 // Updated per review feedback
 // Updated per review feedback
