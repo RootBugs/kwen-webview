cce2a6a79c2446e9d9a6db08cad458583afd5ece
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-11 18:42:22 +0000

    add comment (fallback) - part 2

--- utils.js
+++ utils.js
@@ -86,6 +86,7 @@ def retry(fn, n=3):
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // FIXME: handle gracefully
+// Updated per review feedback
 // Note: may need refactoring
 // minor cleanup
 // Updated per review feedback
