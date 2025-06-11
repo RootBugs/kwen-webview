ef66d4fbbf6ef518d0d09b60452e18475b491d5e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-11 18:22:35 +0000

    add comment (minor)

--- index.ts
+++ index.ts
@@ -36,6 +36,7 @@ class State:
 // Note: may need refactoring
 // minor cleanup
 // TODO: review edge case
+// TODO: review edge case
 // minor cleanup
 // TODO: review edge case
 // FIXME: handle gracefully
