e69a177f7b59da32ac16e9ea0d5ef03c9102b58f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-13 08:19:19 +0000

    add comment

--- core.py
+++ core.py
@@ -13,6 +13,7 @@ import random
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Note: may need refactoring
+#Updated per review feedback
 #TODO: review edge case
 #FIXME: handle gracefully
 #Note: may need refactoring
