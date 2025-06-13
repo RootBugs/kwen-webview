0b5295721d872c2fca12445348007d70b7c0874d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-13 21:01:39 +0000

    add comment (minor)

--- index.ts
+++ index.ts
@@ -43,6 +43,7 @@ class State:
 // TODO: review edge case
     active: bool = True
 // Updated per review feedback
+// FIXME: handle gracefully
 // minor cleanup
 // Updated per review feedback
 // FIXME: handle gracefully
