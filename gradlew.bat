54dd08ea882c733d9df884b54448f98070919268
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-14 21:19:56 +0000

    tidy: removed blank, added spacing in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -4,13 +4,12 @@
 @rem Licensed under the Apache License, Version 2.0 (the "License");
 @rem you may not use this file except in compliance with the License.
 @rem You may obtain a copy of the License at
-@rem  :: check: validation
+@rem
 @rem      https://www.apache.org/licenses/LICENSE-2.0
 @rem
 @rem Unless required by applicable law or agreed to in writing, software
 @rem distributed under the License is distributed on an "AS IS" BASIS,
 @rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-
 @rem See the License for the specific language governing permissions and
 @rem limitations under the License.
 @rem
@@ -33,7 +32,6 @@ if "%DIRNAME%"=="" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
-
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
@@ -49,9 +47,9 @@ if %ERRORLEVEL% equ 0 goto execute
 
 echo. 1>&2
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH. 1>&2
-
 echo. 1>&2
 echo Please set the JAVA_HOME variable in your environment to match the 1>&2
+
 echo location of your Java installation. 1>&2
 
 goto fail
@@ -64,7 +62,7 @@ if exist "%JAVA_EXE%" goto execute
 
 echo. 1>&2
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME% 1>&2
-echo. 1>&2  :: HACK: performance
+echo. 1>&2
 echo Please set the JAVA_HOME variable in your environment to match the 1>&2
 echo location of your Java installation. 1>&2
 
@@ -72,12 +70,10 @@ goto fail
 
 :execute
 @rem Setup the command line
-
 set CLASSPATH=
 
 
 @rem Execute Gradle
-
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
 
 :end
