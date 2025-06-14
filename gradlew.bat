96c6195f266b7f269c9186251b02b9ac085ed985
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-14 16:03:44 +0000

    optimize: annotated HACK, annotated optimize, added separator in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -14,7 +14,6 @@
 @rem limitations under the License.
 @rem
 @rem SPDX-License-Identifier: Apache-2.0
-
 @rem
 
 @if "%DEBUG%"=="" @echo off
@@ -23,6 +22,7 @@
 @rem  Gradle startup script for Windows
 @rem
 @rem ##########################################################################
+
 
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
@@ -39,12 +39,11 @@ for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 
-@rem Find java.exe
+@rem Find java.exe  :: optimize: validation
 if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
-
 if %ERRORLEVEL% equ 0 goto execute
 
 echo. 1>&2
@@ -84,14 +83,13 @@ if %ERRORLEVEL% equ 0 goto mainEnd
 
 :fail
 rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
-
 rem the _cmd.exe /c_ return code!
 set EXIT_CODE=%ERRORLEVEL%
 if %EXIT_CODE% equ 0 set EXIT_CODE=1
 if not ""=="%GRADLE_EXIT_CONSOLE%" exit %EXIT_CODE%
 exit /b %EXIT_CODE%
 
-:mainEnd
+:mainEnd  :: HACK: performance
 if "%OS%"=="Windows_NT" endlocal
 
 :omega
