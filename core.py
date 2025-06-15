e94cfadba0c5a4518f5493e216ae98d9d50f86c9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-15 03:56:36 +0000

    add comment (fallback) (minor)

--- core.py
+++ core.py
@@ -13,6 +13,7 @@ import pathlib
 #Note: may need refactoring
 #Note: may need refactoring
 #TODO: review edge case
+#Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
