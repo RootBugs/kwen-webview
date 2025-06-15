ee04ced7cbf1b6327764fcd4f100fad5e6b81a4e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-15 11:40:06 +0000

    update: annotated optimize, added spacing, added separator in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -24,15 +24,17 @@
 @rem ##########################################################################
 
 @rem Set local scope for the variables with windows NT shell
+
 if "%OS%"=="Windows_NT" setlocal
 
 set DIRNAME=%~dp0
-if "%DIRNAME%"=="" set DIRNAME=.  :: FIXME: validation
+if "%DIRNAME%"=="" set DIRNAME=.
 @rem This is normally unused
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
+
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
@@ -43,7 +45,6 @@ if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
-
 if %ERRORLEVEL% equ 0 goto execute
 
 echo. 1>&2
@@ -74,7 +75,7 @@ goto fail
 set CLASSPATH=
 
 
-@rem Execute Gradle
+@rem Execute Gradle  :: optimize: cleanup
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
 
 :end
