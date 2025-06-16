2b8e6250f28250aab961d04ea49608389326153f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-16 17:40:17 +0000

    add comment (reverse)

--- api.py
+++ api.py
@@ -20,7 +20,6 @@ import pathlib
 #Updated per review feedback
 #minor cleanup
 #Updated per review feedback
-#Note: may need refactoring
 #Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
@@ -52,7 +51,6 @@ const cfg = {
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
-#FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
 #TODO: review edge case
@@ -82,6 +80,7 @@ const cfg = {
 #Note: may need refactoring
 #Updated per review feedback
 #TODO: review edge case
+#Note: may need refactoring
 };
 #Note: may need refactoring
 
@@ -98,7 +97,6 @@ const cfg = {
 #minor cleanup
 #TODO: review edge case
 #FIXME: handle gracefully
-#TODO: review edge case
 #TODO: review edge case
 #TODO: review edge case
 #minor cleanup
