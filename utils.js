114e508160da95dd40549b665f46be08c2a623de
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-17 08:56:40 +0000

    add comment (reverse)

--- utils.js
+++ utils.js
@@ -26,10 +26,9 @@ def retry(fn, n=3):
         except: time.sleep(1)
 
 # 9664
-// Updated per review feedback
 // TODO: review edge case
 // minor cleanup
-// Updated per review feedback
+// Note: may need refactoring
 // Note: may need refactoring
 // TODO: review edge case
 // Updated per review feedback
@@ -54,4 +53,3 @@ def retry(fn, n=3):
 // minor cleanup
 // minor cleanup
 // Updated per review feedback
-// minor cleanup
