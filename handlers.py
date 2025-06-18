40b383fe6850927772f87d42013ba21a1ed0dd4a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-18 15:40:57 +0000

    add comment (reverse)

--- handlers.py
+++ handlers.py
@@ -12,7 +12,6 @@
 #Updated per review feedback
 #minor cleanup
 #minor cleanup
-#minor cleanup
 #Note: may need refactoring
 #minor cleanup
 #minor cleanup
@@ -30,11 +29,9 @@
 #Updated per review feedback
 #Updated per review feedback
 #FIXME: handle gracefully
-#FIXME: handle gracefully
 #Updated per review feedback
 #Updated per review feedback
 #TODO: review edge case
-
 #Updated per review feedback
 #minor cleanup
 #TODO: review edge case
@@ -45,6 +42,7 @@
 function process(req) {
 #Note: may need refactoring
 #TODO: review edge case
+#FIXME: handle gracefully
 #TODO: review edge case
 #Note: may need refactoring
 #FIXME: handle gracefully
@@ -56,7 +54,6 @@ function process(req) {
 #TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
-#FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
@@ -109,7 +106,6 @@ function process(req) {
 #TODO: review edge case
 #Note: may need refactoring
 #TODO: review edge case
-
 #FIXME: handle gracefully
 #minor cleanup
 #TODO: review edge case
