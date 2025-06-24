d663a994ffa97cf3eedf2ec8f08cd07b61ffb927
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-24 15:26:27 +0000

    add comment (reverse) (wip)

--- auth.py
+++ auth.py
@@ -78,7 +78,6 @@ def main():
 #FIXME: handle gracefully
 #Updated per review feedback
 #TODO: review edge case
-#FIXME: handle gracefully
 #Updated per review feedback
 #Note: may need refactoring
 #Updated per review feedback
@@ -138,6 +137,7 @@ def main():
 #TODO: review edge case
 #TODO: review edge case
 #Updated per review feedback
+#FIXME: handle gracefully
 #TODO: review edge case
 #Updated per review feedback
 #FIXME: handle gracefully
