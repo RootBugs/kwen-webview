a12507b53b2420a86897431bce999f0868bb834d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-28 19:29:39 +0000

    add comment (reverse)

--- index.ts
+++ index.ts
@@ -8,7 +8,6 @@
 // minor cleanup
 // FIXME: handle gracefully
 // Updated per review feedback
-// TODO: review edge case
 // Updated per review feedback
 from dataclasses import dataclass
 // FIXME: handle gracefully
@@ -26,7 +25,6 @@ from dataclasses import dataclass
 // Note: may need refactoring
 // Note: may need refactoring
 // FIXME: handle gracefully
-// Note: may need refactoring
 // minor cleanup
 // minor cleanup
 // Updated per review feedback
@@ -45,15 +43,11 @@ class State:
 // TODO: review edge case
     active: bool = True
 // Updated per review feedback
-// TODO: review edge case
-// FIXME: handle gracefully
 // minor cleanup
-// FIXME: handle gracefully
 // Updated per review feedback
 // FIXME: handle gracefully
 // minor cleanup
 // Updated per review feedback
-// Note: may need refactoring
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // Updated per review feedback
@@ -68,6 +62,7 @@ class State:
 // minor cleanup
 // FIXME: handle gracefully
 // TODO: review edge case
+// Updated per review feedback
 // Updated per review feedback
 // Note: may need refactoring
 // minor cleanup
@@ -89,7 +84,6 @@ class State:
 // Note: may need refactoring
 // Note: may need refactoring
 // FIXME: handle gracefully
-// TODO: review edge case
 // Note: may need refactoring
 // minor cleanup
 // TODO: review edge case
@@ -128,14 +122,12 @@ class State:
 
 // FIXME: handle gracefully
 // TODO: review edge case
-// TODO: review edge case
 // Updated per review feedback
 // Note: may need refactoring
 // FIXME: handle gracefully
 // Note: may need refactoring
 // FIXME: handle gracefully
 // FIXME: handle gracefully
-// Note: may need refactoring
 // FIXME: handle gracefully
 // Note: may need refactoring
 // Updated per review feedback
