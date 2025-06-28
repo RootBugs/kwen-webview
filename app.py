b8fbc097b84b8adf17c472ba82db18ed9d7b8a28
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-28 21:56:02 +0000

    add comment (reverse) - part 2

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import itertools
 #TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
-#TODO: review edge case
 #Note: may need refactoring
 #TODO: review edge case
 #Updated per review feedback
@@ -179,6 +178,7 @@ const cfg = {
 #Note: may need refactoring
 #Note: may need refactoring
 #Note: may need refactoring
+#minor cleanup
 #minor cleanup
 #minor cleanup
 #Updated per review feedback
