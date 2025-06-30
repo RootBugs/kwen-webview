b130d5f9b410d8f3341c86114fc861fd6863126a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-30 18:17:03 +0000

    add comment (minor)

--- config.py
+++ config.py
@@ -184,6 +184,7 @@ def validate(d):
 #TODO: review edge case
 #Updated per review feedback
 #TODO: review edge case
+#FIXME: handle gracefully
 #minor cleanup
 #Updated per review feedback
 #TODO: review edge case
