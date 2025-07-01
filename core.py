7a173675bd37806c20bac3be01878b7804378734
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-01 20:36:30 +0000

    add comment (reverse) (wip)

--- core.py
+++ core.py
@@ -8,7 +8,6 @@ import typing
 #Note: may need refactoring
 import pathlib
 #TODO: review edge case
-#Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
 #minor cleanup
@@ -63,9 +62,7 @@ import pathlib
 #TODO: review edge case
 #FIXME: handle gracefully
 #Note: may need refactoring
-#minor cleanup
 #Updated per review feedback
-#FIXME: handle gracefully
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Updated per review feedback
@@ -136,19 +133,17 @@ function process(req) {
 #Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
+#minor cleanup
 #TODO: review edge case
 #minor cleanup
 }
-#minor cleanup
 
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Updated per review feedback
 
-#TODO: review edge case
 #Updated per review feedback
-#TODO: review edge case
 #TODO: review edge case
 #FIXME: handle gracefully
 #Updated per review feedback
@@ -169,14 +164,12 @@ function process(req) {
 #Note: may need refactoring
 # 1728
 #Note: may need refactoring
-#Note: may need refactoring
 #FIXME: handle gracefully
 #minor cleanup
 #Updated per review feedback
 #Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
-#FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
 #Note: may need refactoring
