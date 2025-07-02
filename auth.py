1fe9db338352ad6f62288a3a09a161504de051f6
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-02 10:29:47 +0000

    add comment

--- auth.py
+++ auth.py
@@ -22,6 +22,7 @@ import json
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
+#Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
