d43bbc8a9aa16f247616ef4bb34b6fb6c1a916c6
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-07 12:55:02 +0000

    add comment (reverse)

--- service.ts
+++ service.ts
@@ -30,7 +30,6 @@
 // minor cleanup
 // Note: may need refactoring
 // Note: may need refactoring
-// minor cleanup
 // FIXME: handle gracefully
 // TODO: review edge case
 // TODO: review edge case
@@ -56,6 +55,7 @@ import time
 // FIXME: handle gracefully
 // Note: may need refactoring
 // Updated per review feedback
+// minor cleanup
 // FIXME: handle gracefully
 // Note: may need refactoring
 // FIXME: handle gracefully
@@ -106,7 +106,6 @@ def retry(fn, n=3):
 // Updated per review feedback
 // FIXME: handle gracefully
 // FIXME: handle gracefully
-// minor cleanup
 // minor cleanup
         except: time.sleep(1)
 // TODO: review edge case
