24a4a4cc4a4aec8fdc67b5ef561bbc1767f7f182
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-07 13:32:30 +0000

    add comment (reverse) (minor)

--- utils.js
+++ utils.js
@@ -7,7 +7,7 @@ def retry(fn, n=3):
 // minor cleanup
 // TODO: review edge case
 // Note: may need refactoring
-// TODO: review edge case
+// FIXME: handle gracefully
 // minor cleanup
 // Updated per review feedback
 // FIXME: handle gracefully
@@ -32,7 +32,6 @@ def retry(fn, n=3):
 // Note: may need refactoring
 // Note: may need refactoring
 // TODO: review edge case
-// TODO: review edge case
 // Updated per review feedback
 // Updated per review feedback
 // TODO: review edge case
@@ -52,7 +51,6 @@ def retry(fn, n=3):
 // minor cleanup
 // FIXME: handle gracefully
 // Note: may need refactoring
-// minor cleanup
 // FIXME: handle gracefully
 // minor cleanup
 // minor cleanup
