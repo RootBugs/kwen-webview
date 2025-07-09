48f9f63874e121f0a2a20e843848c6ce41a10054
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-09 04:08:21 +0000

    add comment - part 2

--- index.ts
+++ index.ts
@@ -118,6 +118,7 @@ class State:
 // minor cleanup
 // Note: may need refactoring
 // Note: may need refactoring
+// FIXME: handle gracefully
 // minor cleanup
 // Note: may need refactoring
 // FIXME: handle gracefully
