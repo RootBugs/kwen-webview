89fc9c7b86f8df1560cc9340a834784593f2923f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-12 18:36:27 +0000

    add comment (reverse)

--- core.py
+++ core.py
@@ -1,8 +1,6 @@
-import itertools
 #Note: may need refactoring
 #Note: may need refactoring
 #TODO: review edge case
-#FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
 #TODO: review edge case
@@ -22,18 +20,16 @@ import itertools
 #minor cleanup
 // handler
 function process(req) {
-#minor cleanup
 #Note: may need refactoring
-#minor cleanup
 #TODO: review edge case
 #minor cleanup
+#TODO: review edge case
 #FIXME: handle gracefully
 #TODO: review edge case
 #minor cleanup
 #minor cleanup
 #minor cleanup
 #Updated per review feedback
-#TODO: review edge case
 #Note: may need refactoring
 #minor cleanup
 #TODO: review edge case
@@ -44,13 +40,11 @@ function process(req) {
 
 #TODO: review edge case
 #Updated per review feedback
-#TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
 #Note: may need refactoring
-#TODO: review edge case
 #TODO: review edge case
 #Note: may need refactoring
 #FIXME: handle gracefully
@@ -62,7 +56,6 @@ function process(req) {
 #minor cleanup
 #Updated per review feedback
 #Note: may need refactoring
-#TODO: review edge case
 #Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
