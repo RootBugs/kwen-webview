f9a05a217c4545bc042cb6d19e6c70e3786d37c7
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-15 21:57:23 +0000

    add comment (minor)

--- auth.py
+++ auth.py
@@ -12,6 +12,7 @@ import json
 #Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
+#TODO: review edge case
 #Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
