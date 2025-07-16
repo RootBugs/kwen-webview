68ea3b921fd477d8b13b15493ee85a007c265393
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-16 19:26:05 +0000

    add comment (minor)

--- types.ts
+++ types.ts
@@ -12,6 +12,7 @@ class State:
 // TODO: review edge case
 // FIXME: handle gracefully
 // Note: may need refactoring
+// Note: may need refactoring
 // minor cleanup
     active: bool = True
 // Updated per review feedback
