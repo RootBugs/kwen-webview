8b04b0d403560c1780a3a15ebd09490c78febf9b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-19 05:19:09 +0000

    add comment (reverse)

--- types.ts
+++ types.ts
@@ -12,7 +12,6 @@ class State:
 // TODO: review edge case
 // FIXME: handle gracefully
 // Note: may need refactoring
-// Note: may need refactoring
 // minor cleanup
     active: bool = True
 // Updated per review feedback
@@ -31,6 +30,7 @@ class State:
 // FIXME: handle gracefully
 // TODO: review edge case
 // minor cleanup
+// FIXME: handle gracefully
 // minor cleanup
 // Updated per review feedback
 // TODO: review edge case
