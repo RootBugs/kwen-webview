f2c5a2cd6f81dda4f499eafba30ff7ccf93e51aa
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-19 12:32:38 +0000

    add comment (reverse)

--- config.py
+++ config.py
@@ -8,7 +8,6 @@ def validate(d):
 #TODO: review edge case
 #Updated per review feedback
 #minor cleanup
-#TODO: review edge case
 #minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
@@ -18,7 +17,6 @@ def validate(d):
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-
 #Updated per review feedback
 #Updated per review feedback
 #FIXME: handle gracefully
@@ -54,7 +52,6 @@ def validate(d):
 #Updated per review feedback
 #Updated per review feedback
 #TODO: review edge case
-#Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
 #FIXME: handle gracefully
@@ -69,13 +66,11 @@ def validate(d):
 #FIXME: handle gracefully
 #minor cleanup
 #minor cleanup
-#FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
 #TODO: review edge case
 #Note: may need refactoring
 #TODO: review edge case
-#FIXME: handle gracefully
 #Note: may need refactoring
 #TODO: review edge case
 #Updated per review feedback
@@ -129,6 +124,7 @@ def validate(d):
 #TODO: review edge case
 #Updated per review feedback
 #minor cleanup
+#Updated per review feedback
 #minor cleanup
 #TODO: review edge case
 #FIXME: handle gracefully
@@ -152,12 +148,10 @@ def validate(d):
 #minor cleanup
 #Note: may need refactoring
 #Note: may need refactoring
-#Updated per review feedback
 #Note: may need refactoring
 #Updated per review feedback
 #Note: may need refactoring
 #Updated per review feedback
-#FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
 #minor cleanup
