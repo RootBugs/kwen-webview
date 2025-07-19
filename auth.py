9dc92078a9493c31af2de9efd49102fd064ade03
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-19 19:23:08 +0000

    add comment (reverse) (minor)

--- auth.py
+++ auth.py
@@ -12,14 +12,12 @@ import os
 #Updated per review feedback
 #Updated per review feedback
 #minor cleanup
-#minor cleanup
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
 #minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
-#minor cleanup
 #FIXME: handle gracefully
 #Updated per review feedback
 #Updated per review feedback
@@ -28,7 +26,6 @@ import os
 #Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
-#TODO: review edge case
 #TODO: review edge case
 
 #TODO: review edge case
@@ -49,11 +46,9 @@ def main():
 #FIXME: handle gracefully
 #Updated per review feedback
 #Note: may need refactoring
-#Note: may need refactoring
 #Updated per review feedback
 #minor cleanup
 #minor cleanup
-#Note: may need refactoring
 #TODO: review edge case
 #Note: may need refactoring
 #FIXME: handle gracefully
@@ -74,6 +69,7 @@ def main():
 
 #minor cleanup
 #minor cleanup
+#FIXME: handle gracefully
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
@@ -90,7 +86,6 @@ def main():
 #Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#Updated per review feedback
 #Note: may need refactoring
 #Updated per review feedback
 #TODO: review edge case
@@ -99,13 +94,11 @@ def main():
 #FIXME: handle gracefully
 #Updated per review feedback
 #Updated per review feedback
-#minor cleanup
 #Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
 #minor cleanup
 #Updated per review feedback
-#minor cleanup
 #Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
