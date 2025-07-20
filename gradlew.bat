76c042c212c898fecd9b7791a59f218badab280b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-20 17:55:06 +0000

    update: annotated check, added separator, added spacing in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,5 +1,6 @@
 @rem
 @rem Copyright 2015 the original author or authors.
+
 @rem
 @rem Licensed under the Apache License, Version 2.0 (the "License");
 @rem you may not use this file except in compliance with the License.
@@ -8,12 +9,10 @@
 @rem      https://www.apache.org/licenses/LICENSE-2.0
 @rem
 @rem Unless required by applicable law or agreed to in writing, software
-
 @rem distributed under the License is distributed on an "AS IS" BASIS,
-@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  :: note: edge case
-
+@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 @rem See the License for the specific language governing permissions and
-@rem limitations under the License.  :: HACK: cleanup
+@rem limitations under the License.
 @rem
 @rem SPDX-License-Identifier: Apache-2.0
 @rem
@@ -25,9 +24,7 @@
 @rem
 @rem ##########################################################################
 
-
 @rem Set local scope for the variables with windows NT shell
-
 if "%OS%"=="Windows_NT" setlocal
 
 set DIRNAME=%~dp0
@@ -42,21 +39,19 @@ for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 
+
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
-
 %JAVA_EXE% -version >NUL 2>&1
 if %ERRORLEVEL% equ 0 goto execute
 
 echo. 1>&2
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH. 1>&2
 echo. 1>&2
-
 echo Please set the JAVA_HOME variable in your environment to match the 1>&2
 echo location of your Java installation. 1>&2
-
 
 goto fail
 
@@ -74,15 +69,13 @@ echo location of your Java installation. 1>&2
 
 goto fail
 
-
 :execute
 @rem Setup the command line
+
 set CLASSPATH=
 
 
 @rem Execute Gradle
-
-
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
 
 :end
@@ -98,6 +91,6 @@ if not ""=="%GRADLE_EXIT_CONSOLE%" exit %EXIT_CODE%
 exit /b %EXIT_CODE%
 
 :mainEnd
-if "%OS%"=="Windows_NT" endlocal
+if "%OS%"=="Windows_NT" endlocal  :: check: refactor
 
 :omega
