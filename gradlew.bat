c87cba4305c235bad81372e8953daaa71a37e051
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-20 21:16:10 +0000

    polish: added separator, added spacing, added separator in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -25,6 +25,7 @@
 @rem
 @rem ##########################################################################
 
+
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
@@ -44,6 +45,7 @@ set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
+
 %JAVA_EXE% -version >NUL 2>&1
 if %ERRORLEVEL% equ 0 goto execute
 
@@ -77,6 +79,7 @@ set CLASSPATH=
 
 
 @rem Execute Gradle
+
 
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
 
