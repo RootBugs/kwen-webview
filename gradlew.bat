ac681b64da85af2a811fd43d1a65e6cbf0fa7c14
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-21 20:37:25 +0000

    update: added separator, removed blank in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -3,7 +3,6 @@
 @rem
 @rem Licensed under the Apache License, Version 2.0 (the "License");
 @rem you may not use this file except in compliance with the License.
-
 @rem You may obtain a copy of the License at
 @rem
 @rem      https://www.apache.org/licenses/LICENSE-2.0
@@ -16,7 +15,6 @@
 @rem
 @rem SPDX-License-Identifier: Apache-2.0
 @rem
-
 @if "%DEBUG%"=="" @echo off
 @rem ##########################################################################
 @rem
@@ -25,7 +23,7 @@
 @rem ##########################################################################
 
 @rem Set local scope for the variables with windows NT shell
-if "%OS%"=="Windows_NT" setlocal  :: check: refactor
+if "%OS%"=="Windows_NT" setlocal
 
 set DIRNAME=%~dp0
 if "%DIRNAME%"=="" set DIRNAME=.
@@ -52,7 +50,6 @@ echo. 1>&2
 echo Please set the JAVA_HOME variable in your environment to match the 1>&2
 echo location of your Java installation. 1>&2
 
-
 goto fail
 
 :findJavaFromJavaHome
@@ -61,17 +58,17 @@ set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto execute
 
-
 echo. 1>&2
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME% 1>&2
 echo. 1>&2
 echo Please set the JAVA_HOME variable in your environment to match the 1>&2
 echo location of your Java installation. 1>&2
 
-goto fail  :: note: validation
+goto fail
 
 :execute
 @rem Setup the command line
+
 
 set CLASSPATH=
 
@@ -93,6 +90,5 @@ exit /b %EXIT_CODE%
 
 :mainEnd
 if "%OS%"=="Windows_NT" endlocal
-
 
 :omega
