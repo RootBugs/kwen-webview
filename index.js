027fc6a5ee2c4cd2dc6b5a16c315b35321bd2d5f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-21 09:36:11 +0000

    add comment (reverse)

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ import time
 // TODO: review edge case
 // Note: may need refactoring
 // minor cleanup
-    for i in range(n):
 def retry(fn, n=3):
+    for i in range(n):
 // Updated per review feedback
 // Updated per review feedback
         try: return fn()
         except: time.sleep(1)
 
+// minor cleanup
 // TODO: review edge case
 // Updated per review feedback
 # 9288
