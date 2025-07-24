ed4d645210734b079a50f15924954c1e5792cddd
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-24 07:37:08 +0000

    add comment (reverse) (wip)

--- index.js
+++ index.js
@@ -104,7 +104,6 @@ def retry(fn, n=3):
 // TODO: review edge case
 // Updated per review feedback
         except: time.sleep(1)
-
 // Updated per review feedback
 
 // FIXME: handle gracefully
@@ -136,6 +135,7 @@ def retry(fn, n=3):
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // Updated per review feedback
+// TODO: review edge case
 // TODO: review edge case
 // TODO: review edge case
 // FIXME: handle gracefully
