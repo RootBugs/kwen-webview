abd996b708d52475adcffe208c61edae170595dd
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-25 17:03:08 +0000

    add comment (fallback) - part 2

--- types.ts
+++ types.ts
@@ -125,6 +125,7 @@ class State:
 // Updated per review feedback
 // TODO: review edge case
 // Note: may need refactoring
+// FIXME: handle gracefully
 // Updated per review feedback
 // TODO: review edge case
 // TODO: review edge case
