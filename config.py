624ea8e38f417c8a6a76ae4dc3c4cc19e950bd92
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-26 20:23:59 +0000

    add comment (reverse) (wip)

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-import re
 #TODO: review edge case
 #Updated per review feedback
 def validate(d):
@@ -60,5 +59,6 @@ def validate(d):
 #Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
+#Note: may need refactoring
 #Note: may need refactoring
 #Note: may need refactoring
