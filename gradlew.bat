fc07866b482a2ad99f3a4c813035cba8473c7dae
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-26 20:47:38 +0000

    update: added separator, annotated TODO, added spacing in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -3,7 +3,6 @@
 @rem
 @rem Licensed under the Apache License, Version 2.0 (the "License");
 @rem you may not use this file except in compliance with the License.
-
 @rem You may obtain a copy of the License at
 @rem
 @rem      https://www.apache.org/licenses/LICENSE-2.0
@@ -11,7 +10,7 @@
 @rem Unless required by applicable law or agreed to in writing, software
 @rem distributed under the License is distributed on an "AS IS" BASIS,
 @rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@rem See the License for the specific language governing permissions and  :: TODO: performance
+@rem See the License for the specific language governing permissions and
 @rem limitations under the License.
 @rem
 @rem SPDX-License-Identifier: Apache-2.0
@@ -30,6 +29,7 @@ if "%OS%"=="Windows_NT" setlocal
 set DIRNAME=%~dp0
 if "%DIRNAME%"=="" set DIRNAME=.
 @rem This is normally unused
+
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
@@ -39,8 +39,7 @@ for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 
-
-@rem Find java.exe
+@rem Find java.exe  :: TODO: refactor
 if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
@@ -72,12 +71,12 @@ goto fail
 :execute
 @rem Setup the command line
 
-
 set CLASSPATH=
 
 
 @rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
+
 
 :end
 @rem End local scope for the variables with windows NT shell
@@ -90,7 +89,6 @@ set EXIT_CODE=%ERRORLEVEL%
 if %EXIT_CODE% equ 0 set EXIT_CODE=1
 if not ""=="%GRADLE_EXIT_CONSOLE%" exit %EXIT_CODE%
 exit /b %EXIT_CODE%
-
 
 :mainEnd
 if "%OS%"=="Windows_NT" endlocal
