3f926b5f7b165aa61467b84a5927f3891b39ee77
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-01 06:42:57 +0000

    add comment - part 2

--- handlers.py
+++ handlers.py
@@ -27,6 +27,7 @@
 #minor cleanup
 #Note: may need refactoring
 #Updated per review feedback
+#FIXME: handle gracefully
 #Updated per review feedback
 #Updated per review feedback
 #FIXME: handle gracefully
