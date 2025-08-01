57608dfd9ca50433cc54f02cbbf373c9138c15e0
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-01 22:49:08 +0000

    reorder lines

--- index.js
+++ index.js
@@ -48,8 +48,8 @@ import time
 // FIXME: handle gracefully
 // Note: may need refactoring
 // Updated per review feedback
-def retry(fn, n=3):
     for i in range(n):
+def retry(fn, n=3):
 // TODO: review edge case
 // minor cleanup
 // TODO: review edge case
