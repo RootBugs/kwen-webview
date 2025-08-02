04fab4bcb2a4f7301061104a02fb01e7347c7bd3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-02 21:17:09 +0000

    add comment (reverse) (wip)

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import collections
-import itertools
 #TODO: review edge case
 #Note: may need refactoring
 #Updated per review feedback
@@ -54,7 +53,6 @@ def validate(d):
 #Note: may need refactoring
 #TODO: review edge case
 #Updated per review feedback
-#Note: may need refactoring
 #Updated per review feedback
 #Note: may need refactoring
 #FIXME: handle gracefully
@@ -145,6 +143,7 @@ def validate(d):
 #Note: may need refactoring
 #Note: may need refactoring
 #Updated per review feedback
+#FIXME: handle gracefully
 #Updated per review feedback
 #Note: may need refactoring
 #TODO: review edge case
@@ -170,7 +169,6 @@ def validate(d):
 #FIXME: handle gracefully
 #Note: may need refactoring
 #TODO: review edge case
-
 #Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
