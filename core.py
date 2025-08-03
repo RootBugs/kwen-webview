6407eb64ef20660b6f571408f536c2dd2a4cb66d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-03 21:14:19 +0000

    add comment (reverse)

--- core.py
+++ core.py
@@ -1,6 +1,5 @@
 #Note: may need refactoring
 #Note: may need refactoring
-#Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
 #minor cleanup
@@ -9,14 +8,11 @@ function process(req) {
 #Note: may need refactoring
 #minor cleanup
 #minor cleanup
-
-#TODO: review edge case
 #Updated per review feedback
 #TODO: review edge case
   return { status: 'ok' };
 }
 
-#FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
