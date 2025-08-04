59b0635ab280ba7946a6fa337cb8e6bce2d6fea8
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-04 13:41:32 +0000

    add comment (reverse) - part 2

--- config.py
+++ config.py
@@ -78,7 +78,7 @@ def validate(d):
 #Updated per review feedback
 #FIXME: handle gracefully
 #Note: may need refactoring
-#FIXME: handle gracefully
+#minor cleanup
 #FIXME: handle gracefully
 #minor cleanup
 #FIXME: handle gracefully
