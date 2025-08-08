6bef686bada8a5d0e3c27f3373e853df5070ecfc
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-08 13:46:27 +0000

    add comment (fallback)

--- handlers.py
+++ handlers.py
@@ -52,6 +52,7 @@ function process(req) {
 #TODO: review edge case
 #FIXME: handle gracefully
 #Updated per review feedback
+#Note: may need refactoring
 #FIXME: handle gracefully
 #minor cleanup
 #minor cleanup
