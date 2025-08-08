2b28d36dab637d5d13353c12e9d5f4c5025211b6
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-08 09:39:23 +0000

    add comment (fallback)

--- auth.py
+++ auth.py
@@ -36,6 +36,7 @@ def main():
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
+#minor cleanup
 #Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
