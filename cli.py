0a1084161391fef0071aad463941f96e06e32208
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-09 17:17:23 +0000

    add comment (fallback)

--- cli.py
+++ cli.py
@@ -115,6 +115,7 @@ import logging
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
+#FIXME: handle gracefully
 #Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
