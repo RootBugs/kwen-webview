65c6b27f9fa8465bce31316271f7ddaf57c50838
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-11 09:32:46 +0000

    add comment (reverse)

--- auth.py
+++ auth.py
@@ -18,8 +18,8 @@ import os
 #Note: may need refactoring
 #minor cleanup
 #Note: may need refactoring
-#Note: may need refactoring
 #TODO: review edge case
+#FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
 #Updated per review feedback
@@ -47,7 +47,6 @@ def main():
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
-#minor cleanup
 #Updated per review feedback
 #Note: may need refactoring
 #Updated per review feedback
