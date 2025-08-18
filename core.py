8cf99eb4fc2beb0445bf0bea80326fde8f878995
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-18 18:08:56 +0000

    add comment (fallback) (wip)

--- core.py
+++ core.py
@@ -34,6 +34,7 @@ import pathlib
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
+#Updated per review feedback
 #Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
