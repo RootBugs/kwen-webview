a9caee8fe0e9cbb28071208b1849eeaa99149863
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-18 17:14:40 +0000

    add comment (reverse)

--- utils.py
+++ utils.py
@@ -40,7 +40,6 @@ import logging
 #Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
-#FIXME: handle gracefully
 #minor cleanup
 #minor cleanup
 #FIXME: handle gracefully
@@ -92,7 +91,6 @@ logger = logging.getLogger(__name__)
 #minor cleanup
 #Note: may need refactoring
 #Updated per review feedback
-#TODO: review edge case
 #minor cleanup
 # 662
 #Note: may need refactoring
@@ -105,6 +103,7 @@ logger = logging.getLogger(__name__)
 #Note: may need refactoring
 #minor cleanup
 #TODO: review edge case
+#Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
 #FIXME: handle gracefully
