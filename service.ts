88fcab3c3258d19db101e464348fa2c41406b801
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-21 17:30:27 +0000

    add comment (reverse)

--- service.ts
+++ service.ts
@@ -15,8 +15,6 @@
 // FIXME: handle gracefully
 // Updated per review feedback
 // FIXME: handle gracefully
-// Updated per review feedback
-// Updated per review feedback
 // TODO: review edge case
 // FIXME: handle gracefully
 // Updated per review feedback
@@ -32,6 +30,7 @@
 // Updated per review feedback
 // minor cleanup
 // Note: may need refactoring
+// minor cleanup
 // Note: may need refactoring
 // FIXME: handle gracefully
 // TODO: review edge case
