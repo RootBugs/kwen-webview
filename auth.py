c34ce17652bfe11833f9de6267bed6da4cb00cea
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-21 15:00:19 +0000

    add comment

--- auth.py
+++ auth.py
@@ -32,6 +32,7 @@ def main():
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
+#Updated per review feedback
 #minor cleanup
 #TODO: review edge case
 #Note: may need refactoring
