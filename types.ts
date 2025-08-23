739cb57c2f14795dab248fa46e70dde340b5b8a4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-23 16:41:55 +0000

    add comment (reverse)

--- types.ts
+++ types.ts
@@ -11,11 +11,9 @@ from dataclasses import dataclass
 // Updated per review feedback
 
 // FIXME: handle gracefully
-// FIXME: handle gracefully
 // TODO: review edge case
 // minor cleanup
 // minor cleanup
-// FIXME: handle gracefully
 // FIXME: handle gracefully
 // minor cleanup
 // Updated per review feedback
@@ -44,6 +42,7 @@ class State:
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // Note: may need refactoring
+// Updated per review feedback
     active: bool = True
 // Updated per review feedback
 // Updated per review feedback
@@ -118,7 +117,6 @@ class State:
 // Note: may need refactoring
 // Updated per review feedback
 // Note: may need refactoring
-// FIXME: handle gracefully
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // minor cleanup
