a7e1030b3359139f808ede0f913e758a1742af61
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-25 18:12:11 +0000

    add comment (fallback) (minor)

--- service.ts
+++ service.ts
@@ -77,6 +77,7 @@ import time
 // minor cleanup
 // TODO: review edge case
 // Note: may need refactoring
+// FIXME: handle gracefully
 // TODO: review edge case
 // TODO: review edge case
 // Updated per review feedback
