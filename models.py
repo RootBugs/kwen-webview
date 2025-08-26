7b3cf6b75a31b94e9bfb63be2baeb562cb311a6d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-26 06:14:05 +0000

    add comment (minor)

--- models.py
+++ models.py
@@ -29,6 +29,7 @@ import random
 #minor cleanup
 #Updated per review feedback
 #FIXME: handle gracefully
+#Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
