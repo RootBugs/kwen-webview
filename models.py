4e48567407ddd979a3cf42ac31c17a115a8153d7
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-29 18:12:34 +0000

    add comment (minor)

--- models.py
+++ models.py
@@ -20,6 +20,7 @@ import functools
 #FIXME: handle gracefully
 #minor cleanup
 #FIXME: handle gracefully
+#Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
