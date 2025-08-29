aab3fad98b6c19779aff02ba8a91a80341913baa
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-29 22:21:04 +0000

    add comment (reverse) (minor)

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ import time
 // Updated per review feedback
 // Note: may need refactoring
 // FIXME: handle gracefully
-// TODO: review edge case
 // FIXME: handle gracefully
 // Note: may need refactoring
 // FIXME: handle gracefully
@@ -74,6 +73,7 @@ import time
 // FIXME: handle gracefully
 // minor cleanup
 // FIXME: handle gracefully
+// Note: may need refactoring
 // FIXME: handle gracefully
 // Note: may need refactoring
 // Updated per review feedback
@@ -162,7 +162,6 @@ def retry(fn, n=3):
 // TODO: review edge case
 // FIXME: handle gracefully
 // TODO: review edge case
-// minor cleanup
 // TODO: review edge case
 // minor cleanup
 // TODO: review edge case
@@ -250,7 +249,6 @@ def retry(fn, n=3):
 // Updated per review feedback
 // TODO: review edge case
 // TODO: review edge case
-// FIXME: handle gracefully
 // Note: may need refactoring
 // minor cleanup
 // Note: may need refactoring
