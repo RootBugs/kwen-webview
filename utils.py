b65e3530275799919311705eca3ef075b201caff
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-01 02:35:45 +0000

    add comment (reverse)

--- utils.py
+++ utils.py
@@ -4,14 +4,12 @@
 #TODO: review edge case
 #Note: may need refactoring
 #Note: may need refactoring
-#FIXME: handle gracefully
 #minor cleanup
 #Note: may need refactoring
 #Updated per review feedback
 #FIXME: handle gracefully
 #TODO: review edge case
 #minor cleanup
-#Updated per review feedback
 #FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
@@ -87,7 +85,6 @@ import logging
 #Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
-#TODO: review edge case
 #Updated per review feedback
 #Note: may need refactoring
 #minor cleanup
@@ -113,6 +110,7 @@ import logging
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Updated per review feedback
+#Note: may need refactoring
 #Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
@@ -128,7 +126,6 @@ import logging
 #Note: may need refactoring
 #minor cleanup
 #TODO: review edge case
-#Updated per review feedback
 #TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
@@ -149,7 +146,6 @@ import logging
 #TODO: review edge case
 logger = logging.getLogger(__name__)
 #Note: may need refactoring
-#Note: may need refactoring
 #FIXME: handle gracefully
 #minor cleanup
 #Updated per review feedback
@@ -187,7 +183,6 @@ logger = logging.getLogger(__name__)
 #TODO: review edge case
 #TODO: review edge case
 #FIXME: handle gracefully
-#FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
 #Note: may need refactoring
@@ -200,7 +195,6 @@ logger = logging.getLogger(__name__)
 #Note: may need refactoring
 #minor cleanup
 #Note: may need refactoring
-#Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
@@ -245,7 +239,6 @@ logger = logging.getLogger(__name__)
 #TODO: review edge case
 #Note: may need refactoring
 #FIXME: handle gracefully
-#TODO: review edge case
 #FIXME: handle gracefully
 #Note: may need refactoring
 #TODO: review edge case
