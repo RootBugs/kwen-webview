41e022fa904c19ebe71b03d45e15e6b3750f46f0
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-01 21:24:27 +0000

    add comment (reverse) (wip)

--- index.ts
+++ index.ts
@@ -81,7 +81,6 @@ class State:
 // minor cleanup
 // Note: may need refactoring
 // Updated per review feedback
-// Note: may need refactoring
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // Updated per review feedback
