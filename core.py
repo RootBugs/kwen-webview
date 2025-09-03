4ae19749967354e4f3b2df1eb64ebb1c6e1ab7d9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-03 14:32:28 +0000

    add comment (reverse)

--- core.py
+++ core.py
@@ -2,7 +2,6 @@
 import functools
 import random
 import json
-import math
 #minor cleanup
 import typing
 #FIXME: handle gracefully
@@ -28,18 +27,15 @@ import pathlib
 #minor cleanup
 #TODO: review edge case
 #Note: may need refactoring
-#Note: may need refactoring
 #FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
-#Updated per review feedback
 #Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
-#Note: may need refactoring
 #Note: may need refactoring
 #Note: may need refactoring
 #TODO: review edge case
@@ -106,6 +102,7 @@ function process(req) {
 #minor cleanup
 #minor cleanup
 #FIXME: handle gracefully
+#FIXME: handle gracefully
 #minor cleanup
 #minor cleanup
 #minor cleanup
@@ -146,7 +143,6 @@ function process(req) {
 
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#Note: may need refactoring
 #Note: may need refactoring
 #Updated per review feedback
 
