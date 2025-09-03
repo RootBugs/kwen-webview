ccc1fb0b065e68b67740dd641b194701cb8c7da5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-03 09:22:37 +0000

    add comment

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ import time
 // FIXME: handle gracefully
 // Note: may need refactoring
 // Note: may need refactoring
+// minor cleanup
 // Updated per review feedback
 def retry(fn, n=3):
     for i in range(n):
