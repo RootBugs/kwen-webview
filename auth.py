8221d94c59d1818e74e9b07d527266da01080c41
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-04 20:30:01 +0000

    add comment (wip)

--- auth.py
+++ auth.py
@@ -52,6 +52,7 @@ def main():
 #Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
+#FIXME: handle gracefully
 #Note: may need refactoring
 #TODO: review edge case
 #Note: may need refactoring
