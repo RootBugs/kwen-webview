77d02eabbdc81e003361bdd0a69bdbf339341d71
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-06 18:44:47 +0000

    add comment (minor)

--- utils.js
+++ utils.js
@@ -35,6 +35,7 @@ def retry(fn, n=3):
 // TODO: review edge case
 // FIXME: handle gracefully
 // minor cleanup
+// Updated per review feedback
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // Note: may need refactoring
