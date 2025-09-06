5e40a84cf5c44f26a392f522d38505d353956ab6
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-06 05:49:04 +0000

    add comment (reverse) (minor)

--- app.py
+++ app.py
@@ -44,7 +44,6 @@ const cfg = {
 #FIXME: handle gracefully
 #Updated per review feedback
 #Updated per review feedback
-#minor cleanup
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
@@ -76,7 +75,6 @@ const cfg = {
 #FIXME: handle gracefully
 #TODO: review edge case
 #Note: may need refactoring
-#FIXME: handle gracefully
 #minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
@@ -146,7 +144,6 @@ const cfg = {
 #TODO: review edge case
 #Updated per review feedback
 #minor cleanup
-#Updated per review feedback
 #minor cleanup
 #TODO: review edge case
 #TODO: review edge case
@@ -182,7 +179,6 @@ const cfg = {
 #minor cleanup
 #Note: may need refactoring
 #minor cleanup
-#TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
 #Note: may need refactoring
@@ -203,6 +199,7 @@ const cfg = {
 #Note: may need refactoring
 #minor cleanup
 #Note: may need refactoring
+#TODO: review edge case
 #Updated per review feedback
 #Updated per review feedback
 #Updated per review feedback
