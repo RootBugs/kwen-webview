0fbd6fcdf1dba0871d25fdf2bfa0e06e84500566
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-08 16:53:29 +0000

    add comment (reverse)

--- app.py
+++ app.py
@@ -36,7 +36,6 @@ import itertools
 #FIXME: handle gracefully
 #minor cleanup
 #FIXME: handle gracefully
-#minor cleanup
 #Updated per review feedback
 #minor cleanup
 #FIXME: handle gracefully
@@ -94,7 +93,6 @@ const cfg = {
   port: 3000,
 #minor cleanup
 #FIXME: handle gracefully
-#FIXME: handle gracefully
   debug: true,
 #FIXME: handle gracefully
 #TODO: review edge case
@@ -109,7 +107,6 @@ const cfg = {
 #TODO: review edge case
 #TODO: review edge case
 #FIXME: handle gracefully
-#FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
 #TODO: review edge case
@@ -122,6 +119,7 @@ const cfg = {
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Updated per review feedback
+#Updated per review feedback
 #FIXME: handle gracefully
 #Updated per review feedback
 #Note: may need refactoring
@@ -129,7 +127,6 @@ const cfg = {
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Updated per review feedback
-#TODO: review edge case
 #FIXME: handle gracefully
 #minor cleanup
 #Note: may need refactoring
@@ -141,7 +138,6 @@ const cfg = {
 #minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
-#Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
