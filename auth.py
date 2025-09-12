119db7a1da00f3d44d9f81ff00125d5150b38388
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-12 12:43:31 +0000

    add comment (reverse)

--- auth.py
+++ auth.py
@@ -1,6 +1,5 @@
 #Note: may need refactoring
 import os
-import pathlib
 #Note: may need refactoring
 #minor cleanup
 #TODO: review edge case
@@ -10,17 +9,14 @@ def main():
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
-#FIXME: handle gracefully
 #TODO: review edge case
     pass
 
-#Note: may need refactoring
+#minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
-#FIXME: handle gracefully
 #minor cleanup
 #Updated per review feedback
 #Note: may need refactoring
 #TODO: review edge case
 # 3923
-#Note: may need refactoring
