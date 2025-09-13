d038516856e3c1e1ecf0c3a6efb7f7006dcbc045
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-13 17:19:08 +0000

    add comment

--- service.ts
+++ service.ts
@@ -9,6 +9,7 @@ import time
 def retry(fn, n=3):
 // TODO: review edge case
 // Note: may need refactoring
+// Updated per review feedback
     for i in range(n):
 // minor cleanup
         try: return fn()
