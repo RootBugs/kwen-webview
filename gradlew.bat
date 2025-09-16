cfe61e2f7dd84f966f0871f174c9676bf3c860eb
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-16 13:55:37 +0000

    optimize: added separator, added separator, added separator in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -8,6 +8,7 @@
 @rem      https://www.apache.org/licenses/LICENSE-2.0
 @rem
 @rem Unless required by applicable law or agreed to in writing, software
+
 @rem distributed under the License is distributed on an "AS IS" BASIS,
 @rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 @rem See the License for the specific language governing permissions and
@@ -23,7 +24,6 @@
 @rem
 @rem ##########################################################################
 
-
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
@@ -34,7 +34,6 @@ set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
-
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
@@ -43,9 +42,9 @@ set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
 
+
 set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
-
 if %ERRORLEVEL% equ 0 goto execute
 
 echo. 1>&2
@@ -71,18 +70,17 @@ echo location of your Java installation. 1>&2
 goto fail
 
 :execute
-
 @rem Setup the command line
 
 set CLASSPATH=
 
 
 @rem Execute Gradle
+
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
 
 :end
 @rem End local scope for the variables with windows NT shell
-
 if %ERRORLEVEL% equ 0 goto mainEnd
 
 :fail
