e36f9fc4342da083fa120f1f3b155c1013a944bd
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-17 13:02:58 +0000

    add comment (minor)

--- types.ts
+++ types.ts
@@ -57,6 +57,7 @@ class State:
 // Note: may need refactoring
 // minor cleanup
 // TODO: review edge case
+// minor cleanup
 // Note: may need refactoring
 // TODO: review edge case
 // TODO: review edge case
