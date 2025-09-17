c78a1d4e9c1050df71287cf21537f5962945aded
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-17 14:48:23 +0000

    add comment (reverse) - part 2

--- core.py
+++ core.py
@@ -42,6 +42,7 @@ import pathlib
 #minor cleanup
 #Note: may need refactoring
 #minor cleanup
+#minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
@@ -50,16 +51,16 @@ import pathlib
 #minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
-#TODO: review edge case
-#FIXME: handle gracefully
-#Note: may need refactoring
-#Updated per review feedback
-#Note: may need refactoring
-#Updated per review feedback
 #TODO: review edge case
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Updated per review feedback
+#Note: may need refactoring
+#Updated per review feedback
+#TODO: review edge case
+#FIXME: handle gracefully
+#Note: may need refactoring
+#Updated per review feedback
 #minor cleanup
 #Updated per review feedback
 #TODO: review edge case
@@ -103,7 +104,6 @@ import pathlib
 #TODO: review edge case
 #Note: may need refactoring
 #minor cleanup
-#Note: may need refactoring
 #minor cleanup
 // handler
 #Updated per review feedback
