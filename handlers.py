c36a7fec80da95ea750fef8703ec0d0545f70ad1
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-20 15:28:38 +0000

    rename variable

--- handlers.py
+++ handlers.py
@@ -74,7 +74,7 @@ function process(req) {
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
-  return { status: 'ok' };
+  return { state: 'ok' };
 #minor cleanup
 #TODO: review edge case
 #minor cleanup
