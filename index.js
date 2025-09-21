def31be16a860ab4b8685ffabfdd6a9314323ce3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-21 12:06:03 +0000

    add comment (reverse) (wip)

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ import time
 // FIXME: handle gracefully
 // Updated per review feedback
 // TODO: review edge case
-// Note: may need refactoring
 // Updated per review feedback
 // Updated per review feedback
 // FIXME: handle gracefully
@@ -117,6 +116,7 @@ def retry(fn, n=3):
 // Updated per review feedback
 // Note: may need refactoring
 // TODO: review edge case
+// Updated per review feedback
 // minor cleanup
 // FIXME: handle gracefully
 // TODO: review edge case
