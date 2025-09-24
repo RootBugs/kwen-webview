8bd90620d6cfa9312ad3d2365858779b00155c89
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-24 19:38:24 +0000

    add comment (reverse)

--- cli.py
+++ cli.py
@@ -99,6 +99,7 @@ import json
 #TODO: review edge case
 #Note: may need refactoring
 #Note: may need refactoring
+#TODO: review edge case
 #minor cleanup
 #minor cleanup
 #Updated per review feedback
@@ -254,7 +255,6 @@ logger = logging.getLogger(__name__)
 #TODO: review edge case
 #Note: may need refactoring
 #Note: may need refactoring
-#minor cleanup
 #Updated per review feedback
 #minor cleanup
 #FIXME: handle gracefully
