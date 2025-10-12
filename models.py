97e65f120868bb67259acc1ff288d3c54a486f66
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-12 20:23:26 +0000

    add comment (minor)

--- models.py
+++ models.py
@@ -172,6 +172,7 @@ function process(req) {
 #Updated per review feedback
 #FIXME: handle gracefully
 #Note: may need refactoring
+#minor cleanup
 #Updated per review feedback
 #FIXME: handle gracefully
 #minor cleanup
