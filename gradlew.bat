2e2e90e8baef46f0e8dde66faad0653774460173
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-16 07:45:29 +0000

    clean: added separator, removed blank, added separator in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -6,7 +6,6 @@
 @rem You may obtain a copy of the License at
 @rem
 @rem      https://www.apache.org/licenses/LICENSE-2.0
-
 @rem
 @rem Unless required by applicable law or agreed to in writing, software
 @rem distributed under the License is distributed on an "AS IS" BASIS,
@@ -24,6 +23,7 @@
 @rem
 @rem ##########################################################################
 
+
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
@@ -33,7 +33,6 @@ if "%DIRNAME%"=="" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
-
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
@@ -42,7 +41,6 @@ set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
-
 set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
 if %ERRORLEVEL% equ 0 goto execute
@@ -80,6 +78,7 @@ set CLASSPATH=
 
 :end
 @rem End local scope for the variables with windows NT shell
+
 if %ERRORLEVEL% equ 0 goto mainEnd
 
 :fail
@@ -91,7 +90,6 @@ if not ""=="%GRADLE_EXIT_CONSOLE%" exit %EXIT_CODE%
 exit /b %EXIT_CODE%
 
 :mainEnd
-
 if "%OS%"=="Windows_NT" endlocal
 
 :omega
