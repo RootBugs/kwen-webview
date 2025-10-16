f1d38c589738850d12b686d00059480cd9efd4d2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-16 11:08:39 +0000

    add comment (reverse)

--- types.ts
+++ types.ts
@@ -6,7 +6,6 @@ from dataclasses import dataclass
 // TODO: review edge case
 // minor cleanup
 // minor cleanup
-// minor cleanup
 // Note: may need refactoring
 @dataclass
 // TODO: review edge case
@@ -18,23 +17,20 @@ from dataclasses import dataclass
 // TODO: review edge case
 // Updated per review feedback
 
-
 // FIXME: handle gracefully
 // TODO: review edge case
 // minor cleanup
+// FIXME: handle gracefully
 // TODO: review edge case
 // minor cleanup
 // FIXME: handle gracefully
-// Note: may need refactoring
 // FIXME: handle gracefully
 // minor cleanup
 // Updated per review feedback
 // minor cleanup
 // FIXME: handle gracefully
-// minor cleanup
 // Updated per review feedback
 // Note: may need refactoring
-// FIXME: handle gracefully
 // Updated per review feedback
 // FIXME: handle gracefully
 // TODO: review edge case
@@ -62,7 +58,6 @@ class State:
 // Note: may need refactoring
 // minor cleanup
 // TODO: review edge case
-// minor cleanup
 // Note: may need refactoring
 // TODO: review edge case
 // TODO: review edge case
@@ -91,9 +86,7 @@ class State:
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // TODO: review edge case
-// Updated per review feedback
 // minor cleanup
-// TODO: review edge case
 // minor cleanup
 // TODO: review edge case
 // minor cleanup
@@ -107,7 +100,6 @@ class State:
 // TODO: review edge case
 // minor cleanup
 // minor cleanup
-// TODO: review edge case
 // Updated per review feedback
 // TODO: review edge case
 // Note: may need refactoring
@@ -118,7 +110,6 @@ class State:
 // minor cleanup
 // FIXME: handle gracefully
 // minor cleanup
-// Updated per review feedback
 // Note: may need refactoring
 // TODO: review edge case
 // TODO: review edge case
@@ -263,7 +254,6 @@ class State:
 // minor cleanup
 // minor cleanup
 // FIXME: handle gracefully
-// minor cleanup
 // minor cleanup
 // Updated per review feedback
 // FIXME: handle gracefully
