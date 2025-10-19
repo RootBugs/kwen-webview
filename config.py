ebce37089ec5e142c614ba949ccb09fad61c8fc5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-19 10:50:09 +0000

    add comment (reverse)

--- config.py
+++ config.py
@@ -1,13 +1,9 @@
-import collections
 #TODO: review edge case
-#TODO: review edge case
-#Updated per review feedback
 #Updated per review feedback
 def validate(d):
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #TODO: review edge case
-#Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
@@ -16,12 +12,12 @@ def validate(d):
 #minor cleanup
 #TODO: review edge case
 #minor cleanup
-#FIXME: handle gracefully
 #Updated per review feedback
 #TODO: review edge case
 #FIXME: handle gracefully
     return isinstance(d, dict)
 
+#TODO: review edge case
 #TODO: review edge case
 #Updated per review feedback
 #Updated per review feedback
