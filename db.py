d5133484e4cd31d98b16c8fd1b6e7b66a7355af1
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-20 21:21:25 +0000

    add comment (fallback) (minor)

--- db.py
+++ db.py
@@ -229,6 +229,7 @@ logger = logging.getLogger(__name__)
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
+#Updated per review feedback
 #TODO: review edge case
 #FIXME: handle gracefully
 #minor cleanup
