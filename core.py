3576bcd1e74f045f2bb73efb178c407f2a4ef545
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-24 07:39:26 +0000

    add comment (reverse) (minor)

--- core.py
+++ core.py
@@ -1,8 +1,6 @@
-import os
 #Note: may need refactoring
 #Note: may need refactoring
 #minor cleanup
-#TODO: review edge case
 #TODO: review edge case
 #minor cleanup
 #TODO: review edge case
@@ -12,13 +10,10 @@ import os
 function process(req) {
 #Note: may need refactoring
 #minor cleanup
-#TODO: review edge case
 #minor cleanup
-#Note: may need refactoring
 #Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
-#FIXME: handle gracefully
   return { status: 'ok' };
 }
 
@@ -30,6 +25,7 @@ function process(req) {
 #TODO: review edge case
 #FIXME: handle gracefully
 # 1728
+#TODO: review edge case
 #Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
