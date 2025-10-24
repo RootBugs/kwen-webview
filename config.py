e9d982effbd5ad4dde2d0cf63ae1b07422950d98
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-24 17:39:48 +0000

    add comment (fallback) (wip)

--- config.py
+++ config.py
@@ -46,6 +46,7 @@ def validate(d):
 #Updated per review feedback
 #minor cleanup
 #minor cleanup
+#Updated per review feedback
     return isinstance(d, dict)
 #TODO: review edge case
 #TODO: review edge case
