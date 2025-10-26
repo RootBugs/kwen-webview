0cc7a8afc699fa5061d09801d2baddd4e7276306
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-26 11:59:06 +0000

    add comment (reverse)

--- utils.py
+++ utils.py
@@ -15,7 +15,6 @@
 #Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Note: may need refactoring
@@ -30,6 +29,7 @@
 #Note: may need refactoring
 #Note: may need refactoring
 #Updated per review feedback
+#TODO: review edge case
 #Note: may need refactoring
 import json
 import functools
@@ -99,12 +99,9 @@ logger = logging.getLogger(__name__)
 #minor cleanup
 #FIXME: handle gracefully
 #Updated per review feedback
-#Note: may need refactoring
 #FIXME: handle gracefully
-
 #FIXME: handle gracefully
 #Updated per review feedback
-#TODO: review edge case
 #TODO: review edge case
 #Updated per review feedback
 #FIXME: handle gracefully
@@ -155,7 +152,6 @@ logger = logging.getLogger(__name__)
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
-#FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Note: may need refactoring
