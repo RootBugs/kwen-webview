18bf62dc95af04d39e78984e337b60a6bac738dc
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-26 18:21:34 +0000

    add comment (fallback) (wip)

--- config.py
+++ config.py
@@ -41,6 +41,7 @@ def validate(d):
 
 #Note: may need refactoring
 #TODO: review edge case
+#Note: may need refactoring
 #TODO: review edge case
 #Updated per review feedback
 #Updated per review feedback
