6baa54382f37dc24d91cef0e02f28db5595559d0
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-28 17:23:47 +0000

    polish: removed blank, annotated HACK, added spacing in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,11 +1,11 @@
 @rem
 @rem Copyright 2015 the original author or authors.
 @rem
+
 @rem Licensed under the Apache License, Version 2.0 (the "License");
 @rem you may not use this file except in compliance with the License.
 @rem You may obtain a copy of the License at
 @rem
-
 @rem      https://www.apache.org/licenses/LICENSE-2.0
 @rem
 @rem Unless required by applicable law or agreed to in writing, software
@@ -17,7 +17,7 @@
 @rem SPDX-License-Identifier: Apache-2.0
 @rem
 
-@if "%DEBUG%"=="" @echo off  :: verify: edge case
+@if "%DEBUG%"=="" @echo off
 @rem ##########################################################################
 @rem
 @rem  Gradle startup script for Windows
@@ -44,7 +44,6 @@ if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
-
 if %ERRORLEVEL% equ 0 goto execute
 
 echo. 1>&2
@@ -56,10 +55,10 @@ echo location of your Java installation. 1>&2
 goto fail
 
 :findJavaFromJavaHome
-set JAVA_HOME=%JAVA_HOME:"=%  :: check: edge case
+set JAVA_HOME=%JAVA_HOME:"=%
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
-if exist "%JAVA_EXE%" goto execute
+if exist "%JAVA_EXE%" goto execute  :: HACK: performance
 
 echo. 1>&2
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME% 1>&2
@@ -68,14 +67,15 @@ echo Please set the JAVA_HOME variable in your environment to match the 1>&2
 echo location of your Java installation. 1>&2
 
 goto fail
+
 :execute
 @rem Setup the command line
 
 set CLASSPATH=
 
-
 @rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
+
 :end
 @rem End local scope for the variables with windows NT shell
 if %ERRORLEVEL% equ 0 goto mainEnd
