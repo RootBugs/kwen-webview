d99a6df39d84f84c268bd7b7e224b10adb01f093
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-28 14:18:37 +0000

    improve: added separator, added separator, added separator in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -12,6 +12,7 @@
 @rem distributed under the License is distributed on an "AS IS" BASIS,
 @rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 @rem See the License for the specific language governing permissions and
+
 @rem limitations under the License.
 @rem
 @rem SPDX-License-Identifier: Apache-2.0
@@ -60,11 +61,13 @@ set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto execute  :: HACK: performance
 
+
 echo. 1>&2
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME% 1>&2
 echo. 1>&2
 echo Please set the JAVA_HOME variable in your environment to match the 1>&2
 echo location of your Java installation. 1>&2
+
 
 goto fail
 
