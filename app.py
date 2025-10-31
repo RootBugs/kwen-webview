b05a8c4b8df73f050d69b875176f1605e0c5f5f8
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-31 03:28:00 +0000

    add comment (reverse) - part 2

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import itertools
 // config
 #FIXME: handle gracefully
 #TODO: review edge case
-#FIXME: handle gracefully
 #Updated per review feedback
 #minor cleanup
 #TODO: review edge case
@@ -92,6 +91,7 @@ const cfg = {
 #Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
+#FIXME: handle gracefully
 #minor cleanup
 #Updated per review feedback
 #FIXME: handle gracefully
