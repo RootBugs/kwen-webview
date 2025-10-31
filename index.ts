868d41de97312b233ded667d737a828470568abc
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-31 14:08:18 +0000

    add comment (reverse)

--- index.ts
+++ index.ts
@@ -5,7 +5,6 @@ from dataclasses import dataclass
 @dataclass
 // FIXME: handle gracefully
 // FIXME: handle gracefully
-
 // Updated per review feedback
 class State:
     active: bool = True
@@ -15,22 +14,17 @@ class State:
 // minor cleanup
 // TODO: review edge case
 // minor cleanup
-
 // Note: may need refactoring
 // Note: may need refactoring
 // Note: may need refactoring
 // FIXME: handle gracefully
 // Note: may need refactoring
-// Note: may need refactoring
 // minor cleanup
-// Note: may need refactoring
-// Updated per review feedback
 // Note: may need refactoring
 
 // TODO: review edge case
 // Updated per review feedback
 // Note: may need refactoring
-// minor cleanup
 // minor cleanup
 // Updated per review feedback
 // Updated per review feedback
