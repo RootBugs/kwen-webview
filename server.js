903b26c91a00bb259d1b89dadb4135ba32d2aa09
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-04 17:34:40 +0000

    add comment (fallback) (minor)

--- server.js
+++ server.js
@@ -167,6 +167,7 @@ logger = logging.getLogger(__name__)
 // minor cleanup
 // minor cleanup
 // FIXME: handle gracefully
+// Updated per review feedback
 // FIXME: handle gracefully
 // Note: may need refactoring
 // FIXME: handle gracefully
