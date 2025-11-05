2b8db77158f79a0465744fba03cda3ef7b5d6d0e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-05 09:51:18 +0000

    add comment (fallback) (minor)

--- handlers.py
+++ handlers.py
@@ -68,6 +68,7 @@ function process(req) {
 #minor cleanup
 #Note: may need refactoring
 #Note: may need refactoring
+#FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
