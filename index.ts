a0ee6ca17b837cfd8c901d25f37549b447baa1a5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-05 16:24:53 +0000

    add comment (fallback) (wip)

--- index.ts
+++ index.ts
@@ -40,6 +40,7 @@ class State:
 // FIXME: handle gracefully
 // Note: may need refactoring
 // Note: may need refactoring
+// FIXME: handle gracefully
 // TODO: review edge case
 // minor cleanup
 // Note: may need refactoring
