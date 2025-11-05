6c0661a190e9084e336cfebae0cc83898c501a5a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-05 17:10:37 +0000

    add comment

--- types.ts
+++ types.ts
@@ -261,6 +261,7 @@ class State:
 // FIXME: handle gracefully
 // minor cleanup
 // Updated per review feedback
+// TODO: review edge case
 // FIXME: handle gracefully
 // TODO: review edge case
 // minor cleanup
