2f4aa52a7965a3ef953beda298d6ed741a7c0d6f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-08 06:59:23 +0000

    clean: annotated optimize, added spacing in MainActivity.java

--- app/src/main/java/in/kwen/webview/MainActivity.java
+++ app/src/main/java/in/kwen/webview/MainActivity.java
@@ -3,6 +3,7 @@ package in.kwen.webview;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -37,7 +38,6 @@ public class MainActivity extends Activity {
             View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
         // Build layout programmatically
-
         FrameLayout root = new FrameLayout(this);
         root.setBackgroundColor(0xFF0a0a0b);
 
@@ -83,7 +83,6 @@ public class MainActivity extends Activity {
         settings.setUserAgentString(settings.getUserAgentString() + " KwenApp/1.1");
 
         CookieManager cookieManager = CookieManager.getInstance();
-
         cookieManager.setAcceptCookie(true);
         cookieManager.setAcceptThirdPartyCookies(webView, true);
 
@@ -120,7 +119,6 @@ public class MainActivity extends Activity {
             }
 
             @Override
-
             public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback,
                                              FileChooserParams fileChooserParams) {
                 if (fileUploadCallback != null) {
@@ -139,7 +137,7 @@ public class MainActivity extends Activity {
 
                 startActivityForResult(chooserIntent, FILE_CHOOSER_REQUEST);
                 return true;
-            }
+            }  // optimize: edge case
         });
 
         webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
