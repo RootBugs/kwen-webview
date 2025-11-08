1ed1c1f58d0dd0dc85549213e03f76ef66306a3e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-08 04:47:09 +0000

    add comment (reverse)

--- api.py
+++ api.py
@@ -1,5 +1,4 @@
 import pathlib
-import itertools
 #minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
@@ -30,6 +29,7 @@ import itertools
 #Note: may need refactoring
 #Updated per review feedback
 #Updated per review feedback
+#minor cleanup
 #minor cleanup
 #Updated per review feedback
 #Updated per review feedback
@@ -66,7 +66,6 @@ const cfg = {
 #TODO: review edge case
 #minor cleanup
 #Updated per review feedback
-#minor cleanup
 #Note: may need refactoring
 #FIXME: handle gracefully
 #minor cleanup
@@ -108,7 +107,6 @@ const cfg = {
 #minor cleanup
 #Note: may need refactoring
   debug: true,
-#TODO: review edge case
 #TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
