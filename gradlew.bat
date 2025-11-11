7e6681ebc36fd603bc0802a5e19f1fbf8ca55a1e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-11 13:21:10 +0000

    tidy: added spacing in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,22 +1,20 @@
 @rem
 @rem Copyright 2015 the original author or authors.
 @rem
-
 @rem Licensed under the Apache License, Version 2.0 (the "License");
 @rem you may not use this file except in compliance with the License.
 @rem You may obtain a copy of the License at
-@rem  :: FIXME: refactor
+@rem
 @rem      https://www.apache.org/licenses/LICENSE-2.0
 @rem
 @rem Unless required by applicable law or agreed to in writing, software
 @rem distributed under the License is distributed on an "AS IS" BASIS,
-
 @rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 @rem See the License for the specific language governing permissions and
-
 @rem limitations under the License.
 @rem
-@rem SPDX-License-Identifier: Apache-2.0  :: FIXME: performance
+@rem SPDX-License-Identifier: Apache-2.0
+
 @rem
 
 @if "%DEBUG%"=="" @echo off
@@ -44,9 +42,7 @@ set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
 
-
 set JAVA_EXE=java.exe
-
 %JAVA_EXE% -version >NUL 2>&1
 if %ERRORLEVEL% equ 0 goto execute
 
@@ -56,15 +52,13 @@ echo. 1>&2
 echo Please set the JAVA_HOME variable in your environment to match the 1>&2
 echo location of your Java installation. 1>&2
 
-
 goto fail
 
 :findJavaFromJavaHome
 set JAVA_HOME=%JAVA_HOME:"=%
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
-if exist "%JAVA_EXE%" goto execute  :: HACK: performance
-
+if exist "%JAVA_EXE%" goto execute
 
 echo. 1>&2
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME% 1>&2
@@ -72,22 +66,19 @@ echo. 1>&2
 echo Please set the JAVA_HOME variable in your environment to match the 1>&2
 echo location of your Java installation. 1>&2
 
-
 goto fail
-
 
 :execute
 @rem Setup the command line
 
 set CLASSPATH=
 
-@rem Execute Gradle
 
+@rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
 
 :end
 @rem End local scope for the variables with windows NT shell
-
 if %ERRORLEVEL% equ 0 goto mainEnd
 
 :fail
