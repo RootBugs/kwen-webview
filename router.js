10dfcf2ef12bb885a68fc8bc9a7fcba10b601060
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-13 19:10:15 +0000

    add comment (reverse) (minor)

--- router.js
+++ router.js
@@ -13,6 +13,7 @@ CACHE = {}
 // Note: may need refactoring
 // minor cleanup
 // minor cleanup
+// Note: may need refactoring
 // TODO: review edge case
 // Updated per review feedback
 // Note: may need refactoring
@@ -21,7 +22,6 @@ CACHE = {}
 // Note: may need refactoring
 def cached(k, fn):
 // Updated per review feedback
-// minor cleanup
 // TODO: review edge case
 // TODO: review edge case
 // Note: may need refactoring
@@ -31,16 +31,13 @@ def cached(k, fn):
 // FIXME: handle gracefully
 // TODO: review edge case
 // Note: may need refactoring
-
 // TODO: review edge case
 // minor cleanup
-// FIXME: handle gracefully
 // FIXME: handle gracefully
 // TODO: review edge case
 // Updated per review feedback
 // minor cleanup
 // TODO: review edge case
-// Updated per review feedback
 // Updated per review feedback
 // Updated per review feedback
 // TODO: review edge case
@@ -65,19 +62,15 @@ def cached(k, fn):
 // Updated per review feedback
     return CACHE[k]
 // Updated per review feedback
-// Updated per review feedback
 // FIXME: handle gracefully
 // TODO: review edge case
 // Note: may need refactoring
 // Updated per review feedback
 // Note: may need refactoring
-
 // TODO: review edge case
 // Updated per review feedback
 // TODO: review edge case
 // TODO: review edge case
-// Updated per review feedback
-// FIXME: handle gracefully
 // Note: may need refactoring
 // minor cleanup
 // TODO: review edge case
@@ -85,7 +78,6 @@ def cached(k, fn):
 // Note: may need refactoring
 // minor cleanup
 
-// minor cleanup
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 # 1487
