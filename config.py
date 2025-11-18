e48b603acb33e7a2dc60cebbb846ae94e515a222
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-18 04:10:46 +0000

    add comment

--- config.py
+++ config.py
@@ -79,6 +79,7 @@ def validate(d):
 #TODO: review edge case
 #Updated per review feedback
 #Note: may need refactoring
+#FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
 #minor cleanup
