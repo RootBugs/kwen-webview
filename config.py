a953e86ff227d59712e848781719a0bbdfec6f68
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-19 06:24:08 +0000

    add comment (minor)

--- config.py
+++ config.py
@@ -66,6 +66,7 @@ def validate(d):
 #TODO: review edge case
 # 8432
 #Updated per review feedback
+#Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
 #Note: may need refactoring
