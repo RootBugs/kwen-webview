996f0bac2ea42bb24f9cf7a2446782b2d3adc4d4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-21 16:46:11 +0000

    add comment (minor)

--- utils.js
+++ utils.js
@@ -21,6 +21,7 @@ def retry(fn, n=3):
 // Note: may need refactoring
 // TODO: review edge case
 # 9664
+// FIXME: handle gracefully
 // TODO: review edge case
 // TODO: review edge case
 // TODO: review edge case
