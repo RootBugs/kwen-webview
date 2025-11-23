55ad8bf5115c6aca0a05eadf610877f4e52a620e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-23 20:13:02 +0000

    add comment (reverse)

--- auth.py
+++ auth.py
@@ -24,7 +24,6 @@ import json
 #Updated per review feedback
 #TODO: review edge case
 #minor cleanup
-#minor cleanup
 #Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
@@ -32,7 +31,6 @@ import json
 #FIXME: handle gracefully
 #Note: may need refactoring
 #TODO: review edge case
-
 #FIXME: handle gracefully
 #Note: may need refactoring
 #minor cleanup
@@ -42,6 +40,7 @@ import json
 #FIXME: handle gracefully
 #TODO: review edge case
 #Updated per review feedback
+#FIXME: handle gracefully
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
