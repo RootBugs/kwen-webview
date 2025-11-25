99ac9d218c60a01802982dabb7ba638000e447b5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-25 11:19:12 +0000

    add comment (fallback) (minor)

--- types.ts
+++ types.ts
@@ -12,6 +12,7 @@ class State:
 // minor cleanup
 // TODO: review edge case
 // minor cleanup
+// Note: may need refactoring
 // Updated per review feedback
 // TODO: review edge case
 // FIXME: handle gracefully
