ed086d38d96c07d8f04cecb20e38b6f287d254bb
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-28 09:26:52 +0000

    add comment (reverse)

--- utils.py
+++ utils.py
@@ -9,6 +9,7 @@
 #Updated per review feedback
 #FIXME: handle gracefully
 #TODO: review edge case
+#minor cleanup
 #FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
@@ -165,7 +166,6 @@ logger = logging.getLogger(__name__)
 #FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
-#Note: may need refactoring
 #Note: may need refactoring
 #TODO: review edge case
 #Updated per review feedback
