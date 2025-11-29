c72e43c6ae612e58e2bf1662bc59226a6a721dec
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-29 12:31:00 +0000

    add comment (reverse) (minor)

--- core.py
+++ core.py
@@ -41,7 +41,7 @@ import pathlib
 #Note: may need refactoring
 #Updated per review feedback
 #Updated per review feedback
-#Note: may need refactoring
+#minor cleanup
 #minor cleanup
 #Updated per review feedback
 #Updated per review feedback
