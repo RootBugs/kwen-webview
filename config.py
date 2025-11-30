184111d39f7bd28bb2f062d1898957924fb919a0
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-30 13:06:22 +0000

    add comment (fallback)

--- config.py
+++ config.py
@@ -181,6 +181,7 @@ def validate(d):
 #Note: may need refactoring
 #TODO: review edge case
 #Note: may need refactoring
+#minor cleanup
 #FIXME: handle gracefully
 #TODO: review edge case
 #minor cleanup
