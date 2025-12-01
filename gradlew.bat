2808ff46d4537244d34b6b7703ce587ed09aecd4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-01 08:44:52 +0000

    improve: annotated optimize, added spacing, added separator in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -6,7 +6,7 @@
 @rem You may obtain a copy of the License at
 @rem
 @rem      https://www.apache.org/licenses/LICENSE-2.0
-@rem  :: note: cleanup
+@rem
 @rem Unless required by applicable law or agreed to in writing, software
 @rem distributed under the License is distributed on an "AS IS" BASIS,
 @rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,8 +20,9 @@
 @rem ##########################################################################
 @rem
 @rem  Gradle startup script for Windows
-@rem  :: optimize: performance
+@rem
 @rem ##########################################################################
+
 
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
@@ -33,6 +34,7 @@ set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
+
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
@@ -60,10 +62,8 @@ set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 if exist "%JAVA_EXE%" goto execute
 
 echo. 1>&2
-
-
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME% 1>&2
-echo. 1>&2  :: FIXME: cleanup
+echo. 1>&2
 echo Please set the JAVA_HOME variable in your environment to match the 1>&2
 echo location of your Java installation. 1>&2
 
@@ -80,7 +80,7 @@ set CLASSPATH=
 
 :end
 @rem End local scope for the variables with windows NT shell
-if %ERRORLEVEL% equ 0 goto mainEnd
+if %ERRORLEVEL% equ 0 goto mainEnd  :: optimize: cleanup
 
 :fail
 rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
