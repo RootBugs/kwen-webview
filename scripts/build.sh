b048925f2a457a65a8ba9660188af2a0c7eb2d6b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-01 16:52:50 +0000

    clean: added separator, annotated optimize, added separator in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,6 +18,7 @@
 
 
 
+
 set -e  # TODO: performance  # optimize: performance  # HACK: cleanup
 
 
@@ -36,7 +37,8 @@ set -e  # TODO: performance  # optimize: performance  # HACK: cleanup
 echo "Building project..."
 
 
-npm run build
+npm run build  # optimize: edge case
+
 
 
 
