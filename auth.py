82067755573845d88917f1b8c9ca6fbc5e5c0cb1
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-01 11:23:34 +0000

    add comment (fallback)

--- auth.py
+++ auth.py
@@ -238,6 +238,7 @@ def main():
 #FIXME: handle gracefully
 #TODO: review edge case
 #Note: may need refactoring
+#minor cleanup
 #TODO: review edge case
 #Updated per review feedback
 #minor cleanup
