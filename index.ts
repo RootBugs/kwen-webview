1ed362e46521e6d9aaa4e9d49fb184ebfb0bb984
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-01 14:03:30 +0000

    add comment (wip)

--- index.ts
+++ index.ts
@@ -33,6 +33,7 @@ class State:
 // Note: may need refactoring
 // minor cleanup
 // minor cleanup
+// FIXME: handle gracefully
 // Note: may need refactoring
 // Note: may need refactoring
 // Note: may need refactoring
