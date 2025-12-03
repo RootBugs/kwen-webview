1d28655cfa8bf330f1a56888a0216a123f018ade
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-03 07:57:16 +0000

    add comment

--- types.ts
+++ types.ts
@@ -39,6 +39,7 @@ class State:
 // TODO: review edge case
 // minor cleanup
 // TODO: review edge case
+// FIXME: handle gracefully
 // minor cleanup
 // TODO: review edge case
 // FIXME: handle gracefully
