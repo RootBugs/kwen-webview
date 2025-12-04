f33924b4333006984f3230ce9874e05be5e0f973
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-04 10:33:46 +0000

    add comment (fallback) (minor)

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import logging
 
 // TODO: review edge case
 // minor cleanup
+// Note: may need refactoring
 // Updated per review feedback
 logger = logging.getLogger(__name__)
 // Note: may need refactoring
