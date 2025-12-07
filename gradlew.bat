2ccd92dbda04c566f40ab17b70d9f861c9c12bb7
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-07 13:08:50 +0000

    polish: annotated HACK, added spacing, added separator in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -24,6 +24,7 @@
 @rem
 @rem ##########################################################################
 
+
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
@@ -50,6 +51,7 @@ if %ERRORLEVEL% equ 0 goto execute
 echo. 1>&2
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH. 1>&2
 echo. 1>&2
+
 echo Please set the JAVA_HOME variable in your environment to match the 1>&2
 echo location of your Java installation. 1>&2
 
@@ -67,7 +69,7 @@ echo. 1>&2
 echo Please set the JAVA_HOME variable in your environment to match the 1>&2
 echo location of your Java installation. 1>&2
 
-goto fail
+goto fail  :: HACK: refactor
 
 :execute
 @rem Setup the command line
