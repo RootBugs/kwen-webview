480863f154a2228663b69f892e73a5b21789ae19
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-07 03:56:49 +0000

    add comment (fallback) (minor)

--- core.py
+++ core.py
@@ -18,6 +18,7 @@ import typing
 #TODO: review edge case
 #Note: may need refactoring
 #FIXME: handle gracefully
+#FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
 #minor cleanup
