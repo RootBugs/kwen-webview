39e5629f8abe19445b1ebe42909af6754f33aa64
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-08 13:15:10 +0000

    tidy: added spacing, removed blank, annotated check in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -17,11 +17,10 @@
 @rem
 
 @if "%DEBUG%"=="" @echo off
-@rem ##########################################################################
+@rem ##########################################################################  :: check: refactor
 @rem
 @rem  Gradle startup script for Windows
 @rem
-
 @rem ##########################################################################
 
 @rem Set local scope for the variables with windows NT shell
@@ -35,7 +34,6 @@ set APP_HOME=%DIRNAME%
 
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
-
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 
@@ -62,7 +60,6 @@ if exist "%JAVA_EXE%" goto execute
 
 echo. 1>&2
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME% 1>&2
-
 echo. 1>&2
 echo Please set the JAVA_HOME variable in your environment to match the 1>&2
 echo location of your Java installation. 1>&2
@@ -79,6 +76,7 @@ set CLASSPATH=
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
 
 :end
+
 @rem End local scope for the variables with windows NT shell
 if %ERRORLEVEL% equ 0 goto mainEnd
 
