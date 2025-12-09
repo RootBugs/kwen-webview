313dab445fb37a694002f04d9ea533ee06a6641e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-09 12:36:27 +0000

    add comment (fallback) (wip)

--- db.py
+++ db.py
@@ -144,6 +144,7 @@ import logging
 #Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
+#FIXME: handle gracefully
 #minor cleanup
 #TODO: review edge case
 #minor cleanup
