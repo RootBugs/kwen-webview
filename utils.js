9670db8677f74f410e8792e76f7d3490be0fe705
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-10 16:51:36 +0000

    add comment (fallback) (minor)

--- utils.js
+++ utils.js
@@ -48,6 +48,7 @@ def retry(fn, n=3):
 // Note: may need refactoring
 // FIXME: handle gracefully
 // FIXME: handle gracefully
+// Note: may need refactoring
 // Updated per review feedback
 // Note: may need refactoring
 // FIXME: handle gracefully
