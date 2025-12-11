35bde9bb544e3adecf2bacb4ff6fee7093943f47
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-11 19:59:36 +0000

    add comment

--- types.ts
+++ types.ts
@@ -16,6 +16,7 @@ from dataclasses import dataclass
 // minor cleanup
 // Updated per review feedback
 // minor cleanup
+// FIXME: handle gracefully
 // Note: may need refactoring
 // Updated per review feedback
 // TODO: review edge case
