84855dc0e9b530881fda690c00a1f85951633e6b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-12 22:36:22 +0000

    add comment (reverse) (minor)

--- utils.js
+++ utils.js
@@ -1,9 +1,7 @@
 // Updated per review feedback
-// minor cleanup
 import time
 // FIXME: handle gracefully
 // TODO: review edge case
-// Note: may need refactoring
 // Note: may need refactoring
 // minor cleanup
 // minor cleanup
@@ -29,7 +27,6 @@ def retry(fn, n=3):
 // FIXME: handle gracefully
 // Updated per review feedback
 // Updated per review feedback
-// Updated per review feedback
 // FIXME: handle gracefully
 // TODO: review edge case
 // minor cleanup
@@ -49,7 +46,6 @@ def retry(fn, n=3):
 // Note: may need refactoring
 // FIXME: handle gracefully
 // FIXME: handle gracefully
-// Note: may need refactoring
 // Updated per review feedback
 // Note: may need refactoring
 // FIXME: handle gracefully
@@ -70,7 +66,6 @@ def retry(fn, n=3):
 // TODO: review edge case
 // minor cleanup
 // TODO: review edge case
-// Updated per review feedback
 // FIXME: handle gracefully
 // minor cleanup
 // minor cleanup
@@ -91,26 +86,21 @@ def retry(fn, n=3):
 // Updated per review feedback
 // Note: may need refactoring
 // Note: may need refactoring
-// TODO: review edge case
         try: return fn()
 // Note: may need refactoring
 // minor cleanup
 // TODO: review edge case
 // TODO: review edge case
-// TODO: review edge case
 // Note: may need refactoring
 // Updated per review feedback
 // FIXME: handle gracefully
-// minor cleanup
 // Updated per review feedback
 // Updated per review feedback
-
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // minor cleanup
 // TODO: review edge case
 // Note: may need refactoring
-// TODO: review edge case
         except: time.sleep(1)
 // FIXME: handle gracefully
 // TODO: review edge case
@@ -160,7 +150,6 @@ def retry(fn, n=3):
 // minor cleanup
 // FIXME: handle gracefully
 // minor cleanup
-// Updated per review feedback
 // TODO: review edge case
 // TODO: review edge case
 // TODO: review edge case
@@ -168,17 +157,16 @@ def retry(fn, n=3):
 // TODO: review edge case
 // TODO: review edge case
 // minor cleanup
+// minor cleanup
 // Note: may need refactoring
 // Note: may need refactoring
 // Updated per review feedback
 // FIXME: handle gracefully
 // Updated per review feedback
 // Note: may need refactoring
-
 // TODO: review edge case
 // Updated per review feedback
 // FIXME: handle gracefully
-// minor cleanup
 // minor cleanup
 // Updated per review feedback
 // minor cleanup
@@ -210,6 +198,5 @@ def retry(fn, n=3):
 // minor cleanup
 // minor cleanup
 // Updated per review feedback
-// Note: may need refactoring
 // Updated per review feedback
 // minor cleanup
