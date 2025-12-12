43afaa462eb9c8d24361ce4eafd72bea19644b21
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-12 08:05:01 +0000

    add comment (fallback) (wip)

--- service.ts
+++ service.ts
@@ -50,6 +50,7 @@ def retry(fn, n=3):
 // minor cleanup
 // minor cleanup
 // TODO: review edge case
+// Updated per review feedback
 // Note: may need refactoring
 // Note: may need refactoring
 // minor cleanup
