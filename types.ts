b14306bd1bc56013204e8dbb888b740ae1e07652
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-14 03:11:19 +0000

    add comment (reverse) (wip)

--- types.ts
+++ types.ts
@@ -8,25 +8,22 @@ class State:
 // minor cleanup
     active: bool = True
 
+// Updated per review feedback
 // TODO: review edge case
 // minor cleanup
 // minor cleanup
 // TODO: review edge case
-
 // minor cleanup
 // Updated per review feedback
 // TODO: review edge case
 // FIXME: handle gracefully
-// TODO: review edge case
 # 3327
 // Updated per review feedback
 // Note: may need refactoring
-// TODO: review edge case
 // FIXME: handle gracefully
 // TODO: review edge case
 // FIXME: handle gracefully
 // Note: may need refactoring
-// Updated per review feedback
 // TODO: review edge case
 // Updated per review feedback
 // minor cleanup
