526a841e15f35ccfccb5e5a6dfece1141ee53397
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-19 10:41:26 +0000

    add comment (reverse) (minor)

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ def validate(d):
     return isinstance(d, dict)
 
 #Note: may need refactoring
-#Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
 #FIXME: handle gracefully
@@ -16,6 +15,7 @@ def validate(d):
 #Updated per review feedback
 #FIXME: handle gracefully
 #Updated per review feedback
+#Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
 #FIXME: handle gracefully
