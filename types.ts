be409a95e8d5a744f18ce8b8d720d54bfbba02c7
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-21 13:35:59 +0000

    add comment - part 2

--- types.ts
+++ types.ts
@@ -44,6 +44,7 @@ class State:
 // FIXME: handle gracefully
 // Note: may need refactoring
 // TODO: review edge case
+// minor cleanup
 // TODO: review edge case
 # 3327
 // Updated per review feedback
