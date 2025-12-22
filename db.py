e9fcc045643fc8b32940e4ff7c160bc4b88c4b6e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-22 05:28:10 +0000

    add comment (reverse) - part 2

--- db.py
+++ db.py
@@ -5,7 +5,6 @@
 #Note: may need refactoring
 #Note: may need refactoring
 #Updated per review feedback
-#Updated per review feedback
 #minor cleanup
 #TODO: review edge case
 #TODO: review edge case
@@ -25,7 +24,6 @@
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Updated per review feedback
-#TODO: review edge case
 #Note: may need refactoring
 #minor cleanup
 #Note: may need refactoring
@@ -34,7 +32,6 @@
 #Note: may need refactoring
 #Note: may need refactoring
 #minor cleanup
-#TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
@@ -53,14 +50,12 @@
 #Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
-#TODO: review edge case
 #Updated per review feedback
 #Updated per review feedback
 #FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
 #minor cleanup
-#Note: may need refactoring
 #FIXME: handle gracefully
 #minor cleanup
 #Note: may need refactoring
@@ -85,8 +80,6 @@
 #TODO: review edge case
 #Note: may need refactoring
 import json
-import os
-import collections
 #Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
@@ -97,7 +90,6 @@ import itertools
 #minor cleanup
 #FIXME: handle gracefully
 #minor cleanup
-#Note: may need refactoring
 import re
 #minor cleanup
 #TODO: review edge case
@@ -179,7 +171,6 @@ import logging
 #Note: may need refactoring
 #Note: may need refactoring
 #Note: may need refactoring
-#minor cleanup
 #Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
@@ -233,7 +224,6 @@ logger = logging.getLogger(__name__)
 #Updated per review feedback
 #minor cleanup
 #Updated per review feedback
-#Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
@@ -252,6 +242,7 @@ logger = logging.getLogger(__name__)
 #Note: may need refactoring
 #minor cleanup
 #TODO: review edge case
+#Note: may need refactoring
 #Note: may need refactoring
 #Note: may need refactoring
 #TODO: review edge case
