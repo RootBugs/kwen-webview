669aaf46ecafceb2810d65accd63057acf3b83a6
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-24 15:37:11 +0000

    add comment (reverse)

--- core.py
+++ core.py
@@ -1,6 +1,3 @@
-#minor cleanup
-import typing
-import re
 #Note: may need refactoring
 #Note: may need refactoring
 #TODO: review edge case
@@ -18,9 +15,7 @@ import re
 #minor cleanup
 #minor cleanup
 #TODO: review edge case
-#TODO: review edge case
 #Note: may need refactoring
-#FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
@@ -40,6 +35,7 @@ function process(req) {
 #minor cleanup
 #minor cleanup
 #Updated per review feedback
+#minor cleanup
 #minor cleanup
 #Note: may need refactoring
 #minor cleanup
