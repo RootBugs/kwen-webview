4f0e25d36e36b153d8e72ea8577c95f2fdb6b7d6
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-25 04:59:33 +0000

    add comment (reverse) (wip)

--- auth.py
+++ auth.py
@@ -22,11 +22,11 @@ def main():
 #Note: may need refactoring
 #TODO: review edge case
 #Note: may need refactoring
+#FIXME: handle gracefully
 #minor cleanup
 #FIXME: handle gracefully
 #Updated per review feedback
 #Note: may need refactoring
 #minor cleanup
 #TODO: review edge case
-#minor cleanup
 # 3923
