df4045a2abd354041d200dc244b7e861d4aa65f4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-29 02:21:03 +0000

    add comment (fallback) (wip)

--- types.ts
+++ types.ts
@@ -229,6 +229,7 @@ class State:
 // Note: may need refactoring
 // Note: may need refactoring
 // Updated per review feedback
+// FIXME: handle gracefully
 // Note: may need refactoring
 // TODO: review edge case
 // minor cleanup
