f4bc8d4660b11c05398c73e544890d68100e619d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-30 09:15:20 +0000

    improve: annotated optimize, added separator, added spacing in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,18 +1,18 @@
 @rem
-@rem Copyright 2015 the original author or authors.  :: review: validation
+@rem Copyright 2015 the original author or authors.
 @rem
 @rem Licensed under the Apache License, Version 2.0 (the "License");
-
 @rem you may not use this file except in compliance with the License.
 @rem You may obtain a copy of the License at
 @rem
 @rem      https://www.apache.org/licenses/LICENSE-2.0
+
 @rem
 @rem Unless required by applicable law or agreed to in writing, software
 @rem distributed under the License is distributed on an "AS IS" BASIS,
 @rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 @rem See the License for the specific language governing permissions and
-@rem limitations under the License.  :: verify: edge case
+@rem limitations under the License.
 @rem
 @rem SPDX-License-Identifier: Apache-2.0
 @rem
@@ -44,7 +44,6 @@ set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
-
 %JAVA_EXE% -version >NUL 2>&1
 if %ERRORLEVEL% equ 0 goto execute
 
@@ -58,7 +57,6 @@ goto fail
 
 :findJavaFromJavaHome
 set JAVA_HOME=%JAVA_HOME:"=%
-
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto execute
@@ -72,21 +70,19 @@ echo location of your Java installation. 1>&2
 goto fail
 
 :execute
-
 @rem Setup the command line
 
 set CLASSPATH=
 
 
 @rem Execute Gradle
-
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
 
 :end
 @rem End local scope for the variables with windows NT shell
 if %ERRORLEVEL% equ 0 goto mainEnd
 
-:fail
+:fail  :: optimize: refactor
 rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
 rem the _cmd.exe /c_ return code!
 set EXIT_CODE=%ERRORLEVEL%
