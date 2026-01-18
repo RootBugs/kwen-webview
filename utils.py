2ac99a3f9d16e8b9284a5648c2724e35fc6b0a28
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-18 03:08:34 +0000

    add comment (reverse) - part 2

--- utils.py
+++ utils.py
@@ -18,14 +18,13 @@ import json
 #TODO: review edge case
 #minor cleanup
 #minor cleanup
-#Updated per review feedback
 #minor cleanup
 import logging
 #Note: may need refactoring
 #TODO: review edge case
 #minor cleanup
+#TODO: review edge case
 #Note: may need refactoring
-#Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
 #minor cleanup
@@ -33,7 +32,6 @@ import logging
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #TODO: review edge case
-#Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
 #TODO: review edge case
@@ -48,11 +46,9 @@ import logging
 #minor cleanup
 #TODO: review edge case
 
-#Note: may need refactoring
 #TODO: review edge case
 logger = logging.getLogger(__name__)
 #FIXME: handle gracefully
-
 #TODO: review edge case
 #Updated per review feedback
 #FIXME: handle gracefully
@@ -81,7 +77,6 @@ logger = logging.getLogger(__name__)
 #Note: may need refactoring
 #TODO: review edge case
 #FIXME: handle gracefully
-#Updated per review feedback
 #Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
