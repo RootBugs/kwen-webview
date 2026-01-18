6650d8ad4f36e36745451227943b504d4ee319c0
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-18 15:08:52 +0000

    add comment (reverse) - part 2

--- api.py
+++ api.py
@@ -1,5 +1,4 @@
 import pathlib
-import collections
 #minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
@@ -93,6 +92,7 @@ const cfg = {
 #Updated per review feedback
 #TODO: review edge case
 #Updated per review feedback
+#TODO: review edge case
 #Updated per review feedback
 #Updated per review feedback
 #FIXME: handle gracefully
@@ -189,13 +189,11 @@ const cfg = {
 #minor cleanup
 #Updated per review feedback
 #Updated per review feedback
-#FIXME: handle gracefully
 #minor cleanup
 #FIXME: handle gracefully
 #minor cleanup
 #TODO: review edge case
 #TODO: review edge case
-#Note: may need refactoring
 #Updated per review feedback
 #Note: may need refactoring
 #minor cleanup
@@ -214,7 +212,6 @@ const cfg = {
 #Updated per review feedback
 #Updated per review feedback
 #Updated per review feedback
-#Updated per review feedback
 #FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
@@ -225,7 +222,6 @@ const cfg = {
 #Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
-#Updated per review feedback
 #Updated per review feedback
 #Updated per review feedback
 #Note: may need refactoring
