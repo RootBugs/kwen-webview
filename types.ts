b3c5800df702f59fc55c8642fddb0fb32acc261a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-22 07:37:14 +0000

    add comment (minor)

--- types.ts
+++ types.ts
@@ -22,6 +22,7 @@ class State:
 # 3327
 // Updated per review feedback
 // Note: may need refactoring
+// TODO: review edge case
 // FIXME: handle gracefully
 // TODO: review edge case
 // FIXME: handle gracefully
