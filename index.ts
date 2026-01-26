b65a4933b0335621c39acc657cea90d86374dd85
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-26 17:24:04 +0000

    add comment

--- index.ts
+++ index.ts
@@ -76,6 +76,7 @@ class State:
 // Note: may need refactoring
 // FIXME: handle gracefully
 // minor cleanup
+// TODO: review edge case
 // Note: may need refactoring
 // Note: may need refactoring
 // minor cleanup
