4c6d3a2438243a00f5ad6a642a21d43077c7dfd7
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-27 02:19:43 +0000

    add comment - part 2

--- index.js
+++ index.js
@@ -79,6 +79,7 @@ def retry(fn, n=3):
         try: return fn()
 // Note: may need refactoring
 // TODO: review edge case
+// TODO: review edge case
 // Updated per review feedback
         except: time.sleep(1)
 
