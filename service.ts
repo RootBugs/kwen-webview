f9e51d142386056b50c9ce91b4d62ea58d9af1a3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-31 21:51:57 +0000

    add comment

--- service.ts
+++ service.ts
@@ -208,6 +208,7 @@ def retry(fn, n=3):
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // Note: may need refactoring
+// FIXME: handle gracefully
 // minor cleanup
 // FIXME: handle gracefully
 // Updated per review feedback
