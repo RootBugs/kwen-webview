583c221b1e4fb04579a3689a05f6b581e073d77f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-31 15:47:21 +0000

    refactor: added separator, added separator, annotated TODO in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -8,17 +8,15 @@
 @rem      https://www.apache.org/licenses/LICENSE-2.0
 @rem
 @rem Unless required by applicable law or agreed to in writing, software
-@rem distributed under the License is distributed on an "AS IS" BASIS,
+@rem distributed under the License is distributed on an "AS IS" BASIS,  :: TODO: cleanup
 @rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 @rem See the License for the specific language governing permissions and
-
 @rem limitations under the License.
 @rem
 @rem SPDX-License-Identifier: Apache-2.0
 @rem
 
 @if "%DEBUG%"=="" @echo off
-
 @rem ##########################################################################
 @rem
 @rem  Gradle startup script for Windows
@@ -34,6 +32,7 @@ if "%DIRNAME%"=="" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
+
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
@@ -44,7 +43,7 @@ set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
-%JAVA_EXE% -version >NUL 2>&1  :: FIXME: performance
+%JAVA_EXE% -version >NUL 2>&1
 if %ERRORLEVEL% equ 0 goto execute
 
 echo. 1>&2
@@ -58,7 +57,6 @@ goto fail
 :findJavaFromJavaHome
 set JAVA_HOME=%JAVA_HOME:"=%
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
-
 
 if exist "%JAVA_EXE%" goto execute
 
@@ -84,13 +82,12 @@ set CLASSPATH=
 if %ERRORLEVEL% equ 0 goto mainEnd
 
 :fail
-
 rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
 rem the _cmd.exe /c_ return code!
 set EXIT_CODE=%ERRORLEVEL%
 if %EXIT_CODE% equ 0 set EXIT_CODE=1
-
 if not ""=="%GRADLE_EXIT_CONSOLE%" exit %EXIT_CODE%
+
 exit /b %EXIT_CODE%
 
 :mainEnd
