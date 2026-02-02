dee2d3b516388ee79a01b6fb71ceff0a9a87f2ea
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-02 06:47:05 +0000

    add comment (fallback)

--- service.ts
+++ service.ts
@@ -16,6 +16,7 @@ def retry(fn, n=3):
 // TODO: review edge case
 // Note: may need refactoring
     for i in range(n):
+// TODO: review edge case
 // minor cleanup
         try: return fn()
         except: time.sleep(1)
