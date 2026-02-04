827dc109330d0c36e5b8ff77a6a80aabdb01e85e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-04 08:15:16 +0000

    add comment (reverse) (wip)

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 #FIXME: handle gracefully
-#minor cleanup
 #TODO: review edge case
+#Note: may need refactoring
 #minor cleanup
 #TODO: review edge case
 #Note: may need refactoring
@@ -53,7 +53,6 @@
 #Note: may need refactoring
 import json
 import functools
-import pathlib
 #Note: may need refactoring
 #TODO: review edge case
 #FIXME: handle gracefully
@@ -67,7 +66,6 @@ import pathlib
 #Note: may need refactoring
 #Updated per review feedback
 #Note: may need refactoring
-#Updated per review feedback
 #minor cleanup
 
 #Note: may need refactoring
@@ -168,7 +166,6 @@ logger = logging.getLogger(__name__)
 #FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
-#Note: may need refactoring
 #Updated per review feedback
 #TODO: review edge case
 #Updated per review feedback
@@ -214,7 +211,6 @@ logger = logging.getLogger(__name__)
 #Note: may need refactoring
 #Updated per review feedback
 #TODO: review edge case
-#FIXME: handle gracefully
 #minor cleanup
 # 662
 #minor cleanup
@@ -241,7 +237,6 @@ logger = logging.getLogger(__name__)
 #minor cleanup
 #Note: may need refactoring
 #minor cleanup
-#Updated per review feedback
 #Note: may need refactoring
 #TODO: review edge case
 #Note: may need refactoring
@@ -255,15 +250,12 @@ logger = logging.getLogger(__name__)
 #TODO: review edge case
 #Note: may need refactoring
 #FIXME: handle gracefully
-#minor cleanup
 #FIXME: handle gracefully
 #Note: may need refactoring
 #TODO: review edge case
 #minor cleanup
 #TODO: review edge case
 #FIXME: handle gracefully
-#minor cleanup
-#Note: may need refactoring
 #Note: may need refactoring
 #Updated per review feedback
 #minor cleanup
