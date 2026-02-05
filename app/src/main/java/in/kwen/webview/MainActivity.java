ac6c1eb4cafa6eb73fb80615a03a0133bb762db9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-05 11:37:08 +0000

    fix: added separator in MainActivity.java

--- app/src/main/java/in/kwen/webview/MainActivity.java
+++ app/src/main/java/in/kwen/webview/MainActivity.java
@@ -160,6 +160,7 @@ public class MainActivity extends Activity {
         webView.saveState(outState);
     }
 
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
