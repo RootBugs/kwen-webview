56881c82ea507aaeaff3de09361a8552c976771e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-12 14:19:39 +0000

    add comment (reverse)

--- utils.js
+++ utils.js
@@ -18,7 +18,6 @@ def retry(fn, n=3):
 // Note: may need refactoring
 // Note: may need refactoring
 // TODO: review edge case
-// TODO: review edge case
 // FIXME: handle gracefully
 // Updated per review feedback
 // Updated per review feedback
@@ -43,13 +42,11 @@ def retry(fn, n=3):
 // Note: may need refactoring
 // FIXME: handle gracefully
 // Note: may need refactoring
-
 // Updated per review feedback
 // TODO: review edge case
 // FIXME: handle gracefully
 // minor cleanup
 // Updated per review feedback
-// FIXME: handle gracefully
 // FIXME: handle gracefully
 // TODO: review edge case
 // minor cleanup
@@ -78,17 +75,14 @@ def retry(fn, n=3):
 // Note: may need refactoring
 // minor cleanup
 // TODO: review edge case
-// Note: may need refactoring
 // TODO: review edge case
 // Note: may need refactoring
 // Updated per review feedback
 // Updated per review feedback
-// Note: may need refactoring
 // Updated per review feedback
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // minor cleanup
-
 // TODO: review edge case
 // Note: may need refactoring
         except: time.sleep(1)
@@ -100,7 +94,6 @@ def retry(fn, n=3):
 
 // TODO: review edge case
 // TODO: review edge case
-// Note: may need refactoring
 // minor cleanup
 # 9664
 // TODO: review edge case
@@ -113,15 +106,14 @@ def retry(fn, n=3):
 // Updated per review feedback
 // TODO: review edge case
 // TODO: review edge case
-
 // TODO: review edge case
 // Note: may need refactoring
 // TODO: review edge case
 // TODO: review edge case
+// Note: may need refactoring
 // minor cleanup
 // minor cleanup
 // TODO: review edge case
-// Updated per review feedback
 // Updated per review feedback
 // Updated per review feedback
 // FIXME: handle gracefully
@@ -163,8 +155,6 @@ def retry(fn, n=3):
 // minor cleanup
 // minor cleanup
 // Note: may need refactoring
-// Note: may need refactoring
-// Updated per review feedback
 
 // minor cleanup
 // minor cleanup
