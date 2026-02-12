d7a7bf95e7fda1118d200490d1d7dcf83a478c38
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-12 15:01:30 +0000

    improve: added spacing, removed blank, added spacing in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,6 +1,7 @@
 @rem
 @rem Copyright 2015 the original author or authors.
 @rem
+
 @rem Licensed under the Apache License, Version 2.0 (the "License");
 @rem you may not use this file except in compliance with the License.
 @rem You may obtain a copy of the License at
@@ -59,7 +60,6 @@ set JAVA_HOME=%JAVA_HOME:"=%
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto execute
-
 echo. 1>&2
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME% 1>&2
 echo. 1>&2
@@ -79,6 +79,7 @@ set CLASSPATH=
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
 
 :end
+
 @rem End local scope for the variables with windows NT shell
 if %ERRORLEVEL% equ 0 goto mainEnd
 
