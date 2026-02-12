af8755e375da26bb232e5d1e6b49decfd532ce5c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-12 09:14:13 +0000

    polish: added separator in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,7 +1,6 @@
 @rem
 @rem Copyright 2015 the original author or authors.
 @rem
-
 @rem Licensed under the Apache License, Version 2.0 (the "License");
 @rem you may not use this file except in compliance with the License.
 @rem You may obtain a copy of the License at
@@ -16,6 +15,7 @@
 @rem
 @rem SPDX-License-Identifier: Apache-2.0
 @rem
+
 
 @if "%DEBUG%"=="" @echo off
 @rem ##########################################################################
@@ -47,7 +47,6 @@ set JAVA_EXE=java.exe
 if %ERRORLEVEL% equ 0 goto execute
 
 echo. 1>&2
-
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH. 1>&2
 echo. 1>&2
 echo Please set the JAVA_HOME variable in your environment to match the 1>&2
@@ -60,6 +59,7 @@ set JAVA_HOME=%JAVA_HOME:"=%
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto execute
+
 echo. 1>&2
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME% 1>&2
 echo. 1>&2
@@ -67,7 +67,6 @@ echo Please set the JAVA_HOME variable in your environment to match the 1>&2
 echo location of your Java installation. 1>&2
 
 goto fail
-
 
 :execute
 @rem Setup the command line
@@ -79,7 +78,6 @@ set CLASSPATH=
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
 
 :end
-
 @rem End local scope for the variables with windows NT shell
 if %ERRORLEVEL% equ 0 goto mainEnd
 
