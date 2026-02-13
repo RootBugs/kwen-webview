21676b7ee828398d91d6f12e3d204d4c6b001ca6
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-13 08:30:11 +0000

    add comment

--- core.py
+++ core.py
@@ -10,6 +10,7 @@ import itertools
 #TODO: review edge case
 #Note: may need refactoring
 #FIXME: handle gracefully
+#Updated per review feedback
 #FIXME: handle gracefully
 #minor cleanup
 #minor cleanup
