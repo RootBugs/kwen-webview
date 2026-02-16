6b0121164f5c39986dc284f2f00776c2a87b73fa
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-16 09:21:45 +0000

    add comment (fallback) - part 2

--- utils.js
+++ utils.js
@@ -21,6 +21,7 @@ def retry(fn, n=3):
 // minor cleanup
 // Note: may need refactoring
 // TODO: review edge case
+// Updated per review feedback
 // minor cleanup
 // TODO: review edge case
 // Updated per review feedback
