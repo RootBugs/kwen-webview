70ad2c7fc84a09fac86ac68d8112554e4bd8d1b6
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-17 18:38:34 +0000

    add comment

--- cli.py
+++ cli.py
@@ -131,6 +131,7 @@ import logging
 #FIXME: handle gracefully
 #Note: may need refactoring
 #FIXME: handle gracefully
+#Note: may need refactoring
 #FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
