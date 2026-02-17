7638546283d937130d8f478ff0b7e93921de0b79
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-17 10:59:35 +0000

    add comment (reverse) (wip)

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import collections
-import math
 #TODO: review edge case
 #Note: may need refactoring
 #Updated per review feedback
@@ -33,6 +32,7 @@ def validate(d):
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
+#Note: may need refactoring
 #minor cleanup
 #TODO: review edge case
 #TODO: review edge case
@@ -67,7 +67,6 @@ def validate(d):
 #TODO: review edge case
 #Note: may need refactoring
 #Note: may need refactoring
-#Note: may need refactoring
 #minor cleanup
 #Updated per review feedback
 #minor cleanup
@@ -82,7 +81,6 @@ def validate(d):
 #minor cleanup
 #minor cleanup
 #TODO: review edge case
-#Updated per review feedback
 #minor cleanup
 #Updated per review feedback
 #FIXME: handle gracefully
@@ -172,7 +170,6 @@ def validate(d):
 #Updated per review feedback
 #TODO: review edge case
 #Updated per review feedback
-#TODO: review edge case
 #minor cleanup
 #minor cleanup
 #Updated per review feedback
@@ -181,7 +178,6 @@ def validate(d):
 #Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
-#FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
