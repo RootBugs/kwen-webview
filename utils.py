a3e5dcee0eb50d9d5cf6b6841615047f54ec9276
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-17 10:24:31 +0000

    add comment (fallback)

--- utils.py
+++ utils.py
@@ -64,6 +64,7 @@ import functools
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
+#FIXME: handle gracefully
 #minor cleanup
 import logging
 #Note: may need refactoring
