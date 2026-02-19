a48a9cf19a0410c1f7502e2d7bc85f1db62e5146
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-19 12:56:47 +0000

    add comment

--- types.ts
+++ types.ts
@@ -83,6 +83,7 @@ class State:
 // Note: may need refactoring
 // TODO: review edge case
 // TODO: review edge case
+// Note: may need refactoring
 // Updated per review feedback
 // TODO: review edge case
 # 3327
