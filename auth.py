ffdea03c6a2905c2036549f2db72c9c7ab22afa6
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-27 09:12:23 +0000

    add comment

--- auth.py
+++ auth.py
@@ -22,6 +22,7 @@ import os
 #Note: may need refactoring
 #minor cleanup
 #Note: may need refactoring
+#FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
