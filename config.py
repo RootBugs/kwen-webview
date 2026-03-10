22d5b3773f544f320e800decf6212188d3a014c9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-10 17:55:56 +0000

    add comment (reverse) (wip)

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 import collections
 #TODO: review edge case
-import json
-#TODO: review edge case
 #Note: may need refactoring
 #Updated per review feedback
 #TODO: review edge case
@@ -46,6 +44,7 @@ def validate(d):
 #Updated per review feedback
 #TODO: review edge case
 #FIXME: handle gracefully
+#Updated per review feedback
 #minor cleanup
 #Updated per review feedback
 #minor cleanup
@@ -69,7 +68,6 @@ def validate(d):
 #TODO: review edge case
 #Note: may need refactoring
 #Note: may need refactoring
-#TODO: review edge case
 #minor cleanup
 #Updated per review feedback
 #minor cleanup
@@ -78,7 +76,6 @@ def validate(d):
 #Note: may need refactoring
 #TODO: review edge case
 #Updated per review feedback
-#minor cleanup
 #TODO: review edge case
 #TODO: review edge case
 #FIXME: handle gracefully
@@ -110,7 +107,6 @@ def validate(d):
 #minor cleanup
 #TODO: review edge case
 #TODO: review edge case
-
 #Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
@@ -200,7 +196,6 @@ def validate(d):
 #Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
-
 #TODO: review edge case
 #Note: may need refactoring
 #Note: may need refactoring
