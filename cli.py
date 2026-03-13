036bb1f62c36324e64278580e5b7fd3b5bd6b23b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-13 22:08:21 +0000

    add comment (reverse)

--- cli.py
+++ cli.py
@@ -74,7 +74,6 @@ import logging
 #Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-
 #FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
@@ -99,7 +98,6 @@ import logging
 #Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
@@ -110,6 +108,7 @@ import logging
 #Note: may need refactoring
 #Updated per review feedback
 #minor cleanup
+#FIXME: handle gracefully
 #Note: may need refactoring
 #TODO: review edge case
 
