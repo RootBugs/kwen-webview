15d96707785ccb49fd3ad28f10b7b6373397e333
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-16 10:12:58 +0000

    add comment

--- auth.py
+++ auth.py
@@ -39,6 +39,7 @@ import json
 #Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
+#FIXME: handle gracefully
 #Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
