84ebe398f9c270067893ce974a80c3b2ae66970f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-17 16:17:55 +0000

    add comment (reverse) (minor)

--- auth.py
+++ auth.py
@@ -122,6 +122,7 @@ def main():
 #TODO: review edge case
 #Updated per review feedback
 #TODO: review edge case
+#FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
 #Note: may need refactoring
@@ -171,7 +172,6 @@ def main():
 #TODO: review edge case
 #TODO: review edge case
 #TODO: review edge case
-
 #TODO: review edge case
 #Updated per review feedback
 #minor cleanup
