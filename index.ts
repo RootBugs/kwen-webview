53830e7193e3b8324a3f29b89cd5fd5a9019eb5b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-19 22:58:25 +0000

    add comment (reverse) (minor)

--- index.ts
+++ index.ts
@@ -122,6 +122,7 @@ class State:
 // Note: may need refactoring
 // minor cleanup
 // Updated per review feedback
+// TODO: review edge case
 // Updated per review feedback
 // minor cleanup
 // Note: may need refactoring
@@ -201,7 +202,6 @@ class State:
 // minor cleanup
 // Updated per review feedback
 // Updated per review feedback
-// minor cleanup
 // Note: may need refactoring
 // Updated per review feedback
 // Updated per review feedback
