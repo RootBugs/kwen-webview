9e335a46d06585cd67ff41546b9f0bcbdf7ace63
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-19 06:02:37 +0000

    add comment (reverse)

--- index.ts
+++ index.ts
@@ -46,7 +46,6 @@ class State:
 // TODO: review edge case
     active: bool = True
 // Updated per review feedback
-// Updated per review feedback
 // minor cleanup
 // Updated per review feedback
 // FIXME: handle gracefully
@@ -72,7 +71,6 @@ class State:
 // Updated per review feedback
 // Note: may need refactoring
 // minor cleanup
-// FIXME: handle gracefully
 // Note: may need refactoring
 // TODO: review edge case
 // Note: may need refactoring
@@ -127,6 +125,7 @@ class State:
 // TODO: review edge case
 // Note: may need refactoring
 // minor cleanup
+// FIXME: handle gracefully
 // FIXME: handle gracefully
 // minor cleanup
 // FIXME: handle gracefully
