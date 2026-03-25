a0a11ab4262b1e964a0ccde7f2e658b73a3867b1
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-25 14:48:15 +0000

    add comment (reverse)

--- core.py
+++ core.py
@@ -1,7 +1,6 @@
 #Note: may need refactoring
 import functools
 import random
-import itertools
 #Note: may need refactoring
 import json
 #minor cleanup
@@ -108,7 +107,6 @@ import pathlib
 #Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
-#FIXME: handle gracefully
 #TODO: review edge case
 #Note: may need refactoring
 #TODO: review edge case
@@ -143,7 +141,6 @@ function process(req) {
 #FIXME: handle gracefully
 #minor cleanup
 #TODO: review edge case
-#minor cleanup
 #minor cleanup
 #TODO: review edge case
 #minor cleanup
@@ -228,7 +225,6 @@ function process(req) {
 #Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
-#TODO: review edge case
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
@@ -286,6 +282,7 @@ function process(req) {
 #Note: may need refactoring
 #minor cleanup
 #TODO: review edge case
+#FIXME: handle gracefully
 #Note: may need refactoring
 #Updated per review feedback
 #FIXME: handle gracefully
