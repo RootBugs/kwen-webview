cfe0307f3fffc193cb3e623fa64cef0ab2478407
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-29 12:50:59 +0000

    add comment (fallback) (wip)

--- config.py
+++ config.py
@@ -189,6 +189,7 @@ def validate(d):
 #FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
+#minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
 #Note: may need refactoring
