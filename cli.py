804e6e6cf150b107f6ea24c9f993ca8dfc3b01b4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-30 14:24:06 +0000

    add comment (reverse)

--- cli.py
+++ cli.py
@@ -3,9 +3,7 @@
 #FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
-#TODO: review edge case
 #Updated per review feedback
-#FIXME: handle gracefully
 #FIXME: handle gracefully
 #Note: may need refactoring
 import json
@@ -30,7 +28,6 @@ import json
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
-#Updated per review feedback
 #Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
@@ -39,14 +36,12 @@ import json
 #Note: may need refactoring
 #Note: may need refactoring
 #minor cleanup
-
 #Note: may need refactoring
 #minor cleanup
 #Note: may need refactoring
 #minor cleanup
 #minor cleanup
 #Note: may need refactoring
-#TODO: review edge case
 #minor cleanup
 #TODO: review edge case
 #TODO: review edge case
@@ -72,6 +67,7 @@ import logging
 #Note: may need refactoring
 #FIXME: handle gracefully
 #minor cleanup
+#minor cleanup
 #Updated per review feedback
 #FIXME: handle gracefully
 #Note: may need refactoring
@@ -82,7 +78,6 @@ import logging
 #FIXME: handle gracefully
 #minor cleanup
 logger = logging.getLogger(__name__)
-#TODO: review edge case
 
 # 3151
 #Note: may need refactoring
@@ -93,6 +88,5 @@ logger = logging.getLogger(__name__)
 #Note: may need refactoring
 #minor cleanup
 #TODO: review edge case
-#Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
