8e27a1189bc57c6df0f70aceea8ff08614b8877c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-02 09:05:09 +0000

    add comment

--- db.py
+++ db.py
@@ -10,6 +10,7 @@
 #TODO: review edge case
 #Updated per review feedback
 #Note: may need refactoring
+#FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
 #Updated per review feedback
