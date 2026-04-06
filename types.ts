d76e18bed87f1ff37541d0311d79e24eceeb7f97
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-06 09:17:51 +0000

    add comment (reverse) (minor)

--- types.ts
+++ types.ts
@@ -1,6 +1,5 @@
 // FIXME: handle gracefully
 // FIXME: handle gracefully
-// Note: may need refactoring
 // TODO: review edge case
 from dataclasses import dataclass
 // minor cleanup
@@ -99,6 +98,7 @@ class State:
 // TODO: review edge case
 // Note: may need refactoring
 // minor cleanup
+// FIXME: handle gracefully
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // Note: may need refactoring
