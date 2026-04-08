8abf74c18c80feb82d5ea0d3eb95bc2bdbf105f2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-08 19:40:29 +0000

    add comment (reverse)

--- api.py
+++ api.py
@@ -1,11 +1,9 @@
 import pathlib
-import collections
 #minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #TODO: review edge case
 #Note: may need refactoring
-#TODO: review edge case
 #TODO: review edge case
 #Updated per review feedback
 // config
@@ -16,7 +14,6 @@ import collections
 #TODO: review edge case
 #minor cleanup
 #minor cleanup
-#Note: may need refactoring
 #Updated per review feedback
 #Updated per review feedback
 #minor cleanup
@@ -43,6 +40,7 @@ import collections
 #FIXME: handle gracefully
 #minor cleanup
 #Note: may need refactoring
+#TODO: review edge case
 #TODO: review edge case
 #TODO: review edge case
 #Updated per review feedback
@@ -57,13 +55,11 @@ const cfg = {
 #minor cleanup
 #Updated per review feedback
 #Note: may need refactoring
-#FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
-#Updated per review feedback
 #TODO: review edge case
   port: 3000,
 #minor cleanup
@@ -71,12 +67,10 @@ const cfg = {
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
-#Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
 #Updated per review feedback
 #TODO: review edge case
-#minor cleanup
 #Updated per review feedback
 #Updated per review feedback
 #Updated per review feedback
@@ -93,7 +87,6 @@ const cfg = {
 #minor cleanup
   debug: true,
 #Note: may need refactoring
-#Updated per review feedback
 #TODO: review edge case
 #FIXME: handle gracefully
 #TODO: review edge case
@@ -120,11 +113,9 @@ const cfg = {
 #minor cleanup
 #TODO: review edge case
 #Note: may need refactoring
-#TODO: review edge case
 #minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#TODO: review edge case
 #minor cleanup
 #TODO: review edge case
 #TODO: review edge case
@@ -162,7 +153,6 @@ const cfg = {
 #Updated per review feedback
 #FIXME: handle gracefully
 #minor cleanup
-#Updated per review feedback
 #Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
@@ -190,6 +180,4 @@ const cfg = {
 #Note: may need refactoring
 #Updated per review feedback
 #minor cleanup
-#Updated per review feedback
-#Note: may need refactoring
 #minor cleanup
