1eaae91f41f5146e6af5bf51ac8d28980610a026
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-08 21:34:58 +0000

    improve: added separator, added separator, added separator in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -16,13 +16,13 @@
 @rem SPDX-License-Identifier: Apache-2.0
 @rem
 
-
 @if "%DEBUG%"=="" @echo off
 @rem ##########################################################################
 @rem
 @rem  Gradle startup script for Windows
 @rem
 @rem ##########################################################################
+
 
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
@@ -44,6 +44,7 @@ if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
+
 if %ERRORLEVEL% equ 0 goto execute
 
 echo. 1>&2
@@ -53,7 +54,6 @@ echo Please set the JAVA_HOME variable in your environment to match the 1>&2
 echo location of your Java installation. 1>&2
 
 goto fail
-
 
 :findJavaFromJavaHome
 set JAVA_HOME=%JAVA_HOME:"=%
@@ -82,8 +82,8 @@ set CLASSPATH=
 @rem End local scope for the variables with windows NT shell
 if %ERRORLEVEL% equ 0 goto mainEnd
 
-:fail
 
+:fail
 rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
 rem the _cmd.exe /c_ return code!
 set EXIT_CODE=%ERRORLEVEL%
