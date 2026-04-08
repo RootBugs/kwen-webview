534b84aa7a5d220052f27cd1fa02f3d5446569a4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-08 07:24:32 +0000

    add comment (reverse)

--- router.js
+++ router.js
@@ -3,17 +3,16 @@
 // minor cleanup
 // TODO: review edge case
 // Updated per review feedback
-// minor cleanup
 // TODO: review edge case
 // minor cleanup
 // minor cleanup
 CACHE = {}
 // Note: may need refactoring
+// TODO: review edge case
 // minor cleanup
 // Note: may need refactoring
 // Note: may need refactoring
 // minor cleanup
-// Updated per review feedback
 // minor cleanup
 // Note: may need refactoring
 // TODO: review edge case
@@ -29,8 +28,6 @@ def cached(k, fn):
 // TODO: review edge case
 // TODO: review edge case
 // Note: may need refactoring
-// Note: may need refactoring
-// minor cleanup
 // Updated per review feedback
     if k not in CACHE:
 // Note: may need refactoring
@@ -44,7 +41,6 @@ def cached(k, fn):
 // minor cleanup
 // Updated per review feedback
 // TODO: review edge case
-// TODO: review edge case
 // Updated per review feedback
 // minor cleanup
 // TODO: review edge case
@@ -57,7 +53,6 @@ def cached(k, fn):
 // minor cleanup
 // TODO: review edge case
 // TODO: review edge case
-// Note: may need refactoring
 // minor cleanup
 // Note: may need refactoring
 // TODO: review edge case
@@ -68,7 +63,6 @@ def cached(k, fn):
         CACHE[k] = fn()
 // Updated per review feedback
 // TODO: review edge case
-// Note: may need refactoring
 // minor cleanup
 // Note: may need refactoring
 // FIXME: handle gracefully
@@ -95,7 +89,6 @@ def cached(k, fn):
 // minor cleanup
 
 // FIXME: handle gracefully
-// Updated per review feedback
 // Note: may need refactoring
 // FIXME: handle gracefully
 # 1487
