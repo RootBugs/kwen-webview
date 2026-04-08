5aefcb6ecc283782a2a809d82f648230f17a9532
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-08 15:54:20 +0000

    tidy: removed blank in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -22,7 +22,6 @@
 @rem  Gradle startup script for Windows
 @rem
 @rem ##########################################################################
-
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
