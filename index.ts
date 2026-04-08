05b2476e9d1da42d129ac2f0b7fc2b4c17458376
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-08 13:18:14 +0000

    add comment (wip)

--- index.ts
+++ index.ts
@@ -185,6 +185,7 @@ class State:
 // Note: may need refactoring
 // TODO: review edge case
 // Note: may need refactoring
+// FIXME: handle gracefully
 // minor cleanup
 // FIXME: handle gracefully
 // FIXME: handle gracefully
