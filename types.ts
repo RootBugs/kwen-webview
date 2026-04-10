bbaa866fe097742643bd92dea94607b1eb222f1f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-10 16:40:23 +0000

    add comment

--- types.ts
+++ types.ts
@@ -213,6 +213,7 @@ class State:
 // minor cleanup
 // Note: may need refactoring
 // Note: may need refactoring
+// FIXME: handle gracefully
 // minor cleanup
 // minor cleanup
 // FIXME: handle gracefully
