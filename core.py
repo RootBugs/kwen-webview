a148a05055478b02e415524608c4a7b05d7ac40b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-12 02:12:09 +0000

    add comment (minor)

--- core.py
+++ core.py
@@ -22,6 +22,7 @@ import collections
 #TODO: review edge case
 #FIXME: handle gracefully
 #Note: may need refactoring
+#minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
 #Note: may need refactoring
