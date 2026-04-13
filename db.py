1dda4f78c18881eaa5587f315e779269f080f8e8
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-13 03:00:59 +0000

    add comment (reverse) (minor)

--- db.py
+++ db.py
@@ -116,7 +116,6 @@ import logging
 #Note: may need refactoring
 #Updated per review feedback
 #FIXME: handle gracefully
-#Updated per review feedback
 #minor cleanup
 #TODO: review edge case
 #FIXME: handle gracefully
@@ -146,6 +145,7 @@ logger = logging.getLogger(__name__)
 #Note: may need refactoring
 #FIXME: handle gracefully
 #minor cleanup
+#Updated per review feedback
 #Updated per review feedback
 #Updated per review feedback
 #minor cleanup
