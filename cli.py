f95c0d8d4502f6c92bdfe2e6e92bfa100c493eb5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-14 13:26:44 +0000

    add comment (reverse)

--- cli.py
+++ cli.py
@@ -20,10 +20,10 @@
 #minor cleanup
 #Updated per review feedback
 #FIXME: handle gracefully
+#Updated per review feedback
 #Note: may need refactoring
 #TODO: review edge case
 import json
-import collections
 #Note: may need refactoring
 #TODO: review edge case
 #FIXME: handle gracefully
@@ -67,7 +67,6 @@ import collections
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
-#Note: may need refactoring
 #Updated per review feedback
 #Updated per review feedback
 #FIXME: handle gracefully
@@ -114,7 +113,6 @@ import collections
 #FIXME: handle gracefully
 #Note: may need refactoring
 #minor cleanup
-#Note: may need refactoring
 #Note: may need refactoring
 #minor cleanup
 #TODO: review edge case
@@ -144,7 +142,6 @@ import logging
 #Updated per review feedback
 #minor cleanup
 #minor cleanup
-
 #minor cleanup
 #TODO: review edge case
 #Updated per review feedback
@@ -181,7 +178,6 @@ import logging
 #Note: may need refactoring
 #Updated per review feedback
 #FIXME: handle gracefully
-#Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
 #FIXME: handle gracefully
@@ -204,7 +200,6 @@ import logging
 #Note: may need refactoring
 #Note: may need refactoring
 #Note: may need refactoring
-#Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Updated per review feedback
@@ -214,7 +209,6 @@ import logging
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #TODO: review edge case
@@ -225,7 +219,6 @@ import logging
 #FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
-#TODO: review edge case
 #minor cleanup
 #FIXME: handle gracefully
 #TODO: review edge case
