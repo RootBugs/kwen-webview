b1f141fe6ccb36464efa86200e3e3f0ea4c28302
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-15 08:33:53 +0000

    add comment (reverse) (minor)

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ import time
 // Note: may need refactoring
 // TODO: review edge case
 // Updated per review feedback
+// FIXME: handle gracefully
 // minor cleanup
 // Note: may need refactoring
 // Note: may need refactoring
@@ -52,7 +53,6 @@ def retry(fn, n=3):
 // Note: may need refactoring
 // Updated per review feedback
         try: return fn()
-// Updated per review feedback
 // TODO: review edge case
         except: time.sleep(1)
 
