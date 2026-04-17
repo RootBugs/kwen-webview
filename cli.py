cff3ee0e94ee9edeb141207bd10f1ec2ec8ff00b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-17 03:28:43 +0000

    add comment (minor)

--- cli.py
+++ cli.py
@@ -40,6 +40,7 @@ import json
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
+#FIXME: handle gracefully
 #Updated per review feedback
 #Note: may need refactoring
 #minor cleanup
