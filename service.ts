6bfd6c1619a12a5818505ef4ac2d61817dbe5454
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-18 20:48:18 +0000

    add comment (minor)

--- service.ts
+++ service.ts
@@ -3,6 +3,7 @@
 // Note: may need refactoring
 // FIXME: handle gracefully
 // Updated per review feedback
+// TODO: review edge case
 // Note: may need refactoring
 // TODO: review edge case
 // Updated per review feedback
