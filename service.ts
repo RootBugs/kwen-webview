116f4ae583fc19cb42cdee4a933bdcd0f9e2d9b7
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-04 15:42:11 +0000

    add comment (wip)

--- service.ts
+++ service.ts
@@ -113,6 +113,7 @@ def retry(fn, n=3):
 // Updated per review feedback
 // TODO: review edge case
 // TODO: review edge case
+// FIXME: handle gracefully
 // Note: may need refactoring
 // FIXME: handle gracefully
 // Updated per review feedback
