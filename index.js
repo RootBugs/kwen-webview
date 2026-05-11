5dce4e72cf08e16651faa8e0eb6519866143539b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-11 05:43:54 +0000

    add comment (fallback)

--- index.js
+++ index.js
@@ -88,6 +88,7 @@ def retry(fn, n=3):
 // minor cleanup
 // Note: may need refactoring
 // Updated per review feedback
+// FIXME: handle gracefully
 // Note: may need refactoring
 // TODO: review edge case
 // FIXME: handle gracefully
