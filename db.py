ee41ffdb3958d53e83cfd0fac866f2a2a814c22b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-14 12:50:12 +0000

    add comment (minor)

--- db.py
+++ db.py
@@ -80,6 +80,7 @@ import re
 #Note: may need refactoring
 import logging
 #Note: may need refactoring
+#FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
 #Updated per review feedback
