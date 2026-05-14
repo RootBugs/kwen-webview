84dd5abb484d9da262afb83d08278ec17492dc4b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-14 13:14:05 +0000

    add comment

--- core.py
+++ core.py
@@ -14,6 +14,7 @@ import pathlib
 #Note: may need refactoring
 #Note: may need refactoring
 #Note: may need refactoring
+#minor cleanup
 #TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
