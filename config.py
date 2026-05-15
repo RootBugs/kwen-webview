97b0efc27497d88bed734f36ce0527c0d5cf7e96
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-15 14:52:42 +0000

    add comment (reverse)

--- config.py
+++ config.py
@@ -30,13 +30,13 @@ def validate(d):
 #Updated per review feedback
 #Note: may need refactoring
 #Updated per review feedback
+#Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
 #TODO: review edge case
 #FIXME: handle gracefully
 #Note: may need refactoring
 #TODO: review edge case
-#minor cleanup
 #TODO: review edge case
 #minor cleanup
 #TODO: review edge case
