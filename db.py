a72578b9f9a006b9d323c392b99c15fa6d83daa6
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-15 10:57:39 +0000

    add comment (reverse) (minor)

--- db.py
+++ db.py
@@ -12,16 +12,14 @@ import json
 #Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
-#minor cleanup
-#minor cleanup
 #Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
 #minor cleanup
 #minor cleanup
+#Note: may need refactoring
 #Updated per review feedback
 import logging
-#Updated per review feedback
 #Note: may need refactoring
 #minor cleanup
 
@@ -46,4 +44,3 @@ logger = logging.getLogger(__name__)
 #Note: may need refactoring
 #Note: may need refactoring
 #TODO: review edge case
-#Updated per review feedback
