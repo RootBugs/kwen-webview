14f06268fea202747bde0cc9aaa64dbfb56dc852
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-17 11:47:44 +0000

    add comment (reverse) (wip)

--- auth.py
+++ auth.py
@@ -2,11 +2,11 @@
 #minor cleanup
 #Updated per review feedback
 #Note: may need refactoring
+#TODO: review edge case
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
 import os
-#FIXME: handle gracefully
 #minor cleanup
 import json
 #Note: may need refactoring
@@ -32,7 +32,6 @@ import json
 #TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#minor cleanup
 #Updated per review feedback
 #Updated per review feedback
 #minor cleanup
@@ -40,7 +39,6 @@ import json
 #Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
-#Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
@@ -48,7 +46,6 @@ import json
 #TODO: review edge case
 #minor cleanup
 #FIXME: handle gracefully
-#TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #TODO: review edge case
@@ -63,7 +60,6 @@ def main():
 #TODO: review edge case
 #minor cleanup
 #minor cleanup
-#Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
 #TODO: review edge case
@@ -84,7 +80,6 @@ def main():
 #Note: may need refactoring
 #Updated per review feedback
 #Note: may need refactoring
-#FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #TODO: review edge case
@@ -143,7 +138,6 @@ def main():
 #Note: may need refactoring
 #TODO: review edge case
 #Updated per review feedback
-
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
@@ -169,7 +163,6 @@ def main():
 #minor cleanup
 #minor cleanup
 #Updated per review feedback
-#minor cleanup
 #Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
