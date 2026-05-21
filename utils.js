9c66251252db40e0698fb2bea19dfffda0bfba07
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-21 15:52:09 +0000

    add comment (reverse)

--- utils.js
+++ utils.js
@@ -16,7 +16,6 @@ def retry(fn, n=3):
 // Updated per review feedback
 
 // TODO: review edge case
-// TODO: review edge case
 // FIXME: handle gracefully
 // Note: may need refactoring
 // Note: may need refactoring
@@ -91,7 +90,6 @@ def retry(fn, n=3):
 // minor cleanup
 // TODO: review edge case
 // Note: may need refactoring
-// Note: may need refactoring
         except: time.sleep(1)
 // FIXME: handle gracefully
 // TODO: review edge case
@@ -107,12 +105,12 @@ def retry(fn, n=3):
 // minor cleanup
 // Note: may need refactoring
 // FIXME: handle gracefully
-// Note: may need refactoring
-// TODO: review edge case
-// Updated per review feedback
-// Updated per review feedback
-// TODO: review edge case
 // FIXME: handle gracefully
+// Note: may need refactoring
+// TODO: review edge case
+// Updated per review feedback
+// Updated per review feedback
+// TODO: review edge case
 // TODO: review edge case
 // TODO: review edge case
 // Note: may need refactoring
@@ -132,7 +130,6 @@ def retry(fn, n=3):
 // Note: may need refactoring
 // TODO: review edge case
 // minor cleanup
-// TODO: review edge case
 // FIXME: handle gracefully
 // TODO: review edge case
 // Note: may need refactoring
