bfce0a421189202cebee212d4438a1c0c608266a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-23 21:47:46 +0000

    add comment (reverse) (wip)

--- server.js
+++ server.js
@@ -62,7 +62,6 @@ import logging
 // Updated per review feedback
 // Note: may need refactoring
 // FIXME: handle gracefully
-// FIXME: handle gracefully
 // TODO: review edge case
 // FIXME: handle gracefully
 // FIXME: handle gracefully
@@ -226,10 +225,10 @@ logger = logging.getLogger(__name__)
 // Note: may need refactoring
 // FIXME: handle gracefully
 // Updated per review feedback
-// Updated per review feedback
 // TODO: review edge case
 // FIXME: handle gracefully
 // FIXME: handle gracefully
+// TODO: review edge case
 // TODO: review edge case
 // TODO: review edge case
 // minor cleanup
