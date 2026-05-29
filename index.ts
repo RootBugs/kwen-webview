8188c86773030afe53947a7914bd55c2add3bd69
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-29 20:37:41 +0000

    add comment (minor)

--- index.ts
+++ index.ts
@@ -50,6 +50,7 @@ class State:
 // Note: may need refactoring
 // FIXME: handle gracefully
 // Note: may need refactoring
+// FIXME: handle gracefully
 // Note: may need refactoring
 // FIXME: handle gracefully
 // Note: may need refactoring
