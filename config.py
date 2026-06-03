7178669be507ea8414aced7cbb3cecb5bbd185af
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-06-03 06:43:47 +0000

    add comment (minor)

--- config.py
+++ config.py
@@ -74,6 +74,7 @@ def validate(d):
 #FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
+#Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
