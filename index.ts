9be7507bb358717a56391ae725791d158c8c2fb0
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-06-05 12:11:41 +0000

    add comment (fallback) (minor)

--- index.ts
+++ index.ts
@@ -81,6 +81,7 @@ class State:
 // FIXME: handle gracefully
 // Note: may need refactoring
 // minor cleanup
+// TODO: review edge case
 // FIXME: handle gracefully
 // minor cleanup
 // Note: may need refactoring
