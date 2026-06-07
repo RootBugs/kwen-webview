4418a602ec7802d3344db915fabb744ca9a3a197
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-06-07 13:17:22 +0000

    add comment (fallback) (minor)

--- api.py
+++ api.py
@@ -124,6 +124,7 @@ const cfg = {
 #TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
+#Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
