9d76647a2407d8e8c7ba9c70f72e02ba69ada86d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-06-09 12:15:26 +0000

    add comment

--- db.py
+++ db.py
@@ -175,6 +175,7 @@ logger = logging.getLogger(__name__)
 #Note: may need refactoring
 #TODO: review edge case
 #FIXME: handle gracefully
+#Updated per review feedback
 #FIXME: handle gracefully
 #TODO: review edge case
 #minor cleanup
